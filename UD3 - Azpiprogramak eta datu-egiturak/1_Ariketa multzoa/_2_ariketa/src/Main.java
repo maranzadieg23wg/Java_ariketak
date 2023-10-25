a4b239b612d9391c4c292c3e481949a10044e7c6
MANUEL <iarrizabaloai23wg@ikzubirimanteo.com>
2023-10-25 09:33:42 +0200

    2.ariketa asi egin naiz, baina ez du funtzionatzen, beste egun batean jarraitzen det

--- UD3 - Azpiprogramak eta datu-egiturak/1_Ariketa multzoa/_2_ariketa/src/Main.java
+++ UD3 - Azpiprogramak eta datu-egiturak/1_Ariketa multzoa/_2_ariketa/src/Main.java
@@ -5,8 +5,51 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         System.out.println("Sartu zenbaki oso bat:");
-        int zenbat = sc.nextInt();
+        int zenbakia = sc.nextInt();
         sc.close();
 
+        int deskonposizioa = deskonposizioa(zenbakia,2);
+
+
+
+
+    }
+
+    public static boolean primo (int z1){
+        boolean primo = false;
+
+        for (int i = 2;i < z1; i++){
+            if (z1 % i == 0){
+                primo =false;
+                break;
+            }
+        }
+
+        return primo;
+
+    }
+
+    public static int deskonposizioa (int z1, int zPrimo){
+
+        boolean primo = false;
+
+        while (z1 != 1){
+            if (z1 % zPrimo == 0){
+                System.out.println(zPrimo);
+                z1 = z1 / zPrimo;
+            }
+            else {
+                while (primo == false){
+                    zPrimo ++;
+                    primo = primo(zPrimo);
+                }
+                deskonposizioa(z1, zPrimo);
+
+            }
+        }
+
+
+
+        return z1;
     }
 }
