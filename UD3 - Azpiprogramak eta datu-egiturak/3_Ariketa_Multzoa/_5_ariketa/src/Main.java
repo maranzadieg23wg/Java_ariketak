2a2b940c89409a37a4c7e7d565b8cab14b6373aa
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2023-10-30 20:36:09 +0100

    UD3/3/_5_ariketa ongi funtzionatzen du, konpondu dut zegoen arazoa konprobatzean ea zenbakiak desordenatuta zeuden a la ez

--- UD3 - Azpiprogramak eta datu-egiturak/3_Ariketa_Multzoa/_5_ariketa/src/Main.java
+++ UD3 - Azpiprogramak eta datu-egiturak/3_Ariketa_Multzoa/_5_ariketa/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -6,6 +7,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int [] zenbakiak = new int[10];
+        int [] kopia = new int[10];
 
         //ordenatuta
         boolean ordenatuta = false;
@@ -21,29 +23,48 @@ public class Main {
         for (int i = 0;i<zenbakiak.length;i++){
             int zenb = sc.nextInt();
             zenbakiak [i] = zenb;
+            kopia [i] = zenb;
         }
 
         for (int i =1;i<zenbakiak.length;i++) {
-            if (zenbakiak[i] > zenbakiak[i - 1]) {
+            if (zenbakiak[i] > zenbakiak[i - 1] && !alderantziz) {
                 ordenatuta = true;
+
+
+
             }
-            if (zenbakiak[i] < zenbakiak[i - 1]) {
+
+            if (zenbakiak[i] < zenbakiak[i - 1] && !ordenatuta) {
                 alderantziz = true;
+
+
+
             }
             if (zenbakiak[i] == zenbakiak[0]) {
                 berdinak = true;
             }
 
 
+
         }
-        if (ordenatuta) {
+        Arrays.sort(kopia);
+        for (int i =0;i<zenbakiak.length;i++) {
+            if (zenbakiak[i] != kopia[i] && zenbakiak[i] != kopia[9-i]){
+                ez_ordenatuta = true;
+                i = zenbakiak.length;
+            }
+        }
+
+        if (ez_ordenatuta){
+            System.out.println("Array-a desordenatuta dago.");
+        } else if (ordenatuta) {
             System.out.println("Array-a goraka ordenatuta dago.");
         } else if (alderantziz) {
             System.out.println("Array-a beheraka ordenatuta dago");
         } else if (berdinak){
             System.out.println("Zenbaki guztiak berdina dira, ezin da esan ordenaturik dagoen.");
         } else {
-            System.out.println("Array-a desordenatuta dago.");
+           System.out.println("ERROR");
         }
 
         sc.close();
