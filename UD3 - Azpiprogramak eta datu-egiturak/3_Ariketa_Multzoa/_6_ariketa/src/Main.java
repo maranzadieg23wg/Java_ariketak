9cfbd214fe9eeba9912020b99986e6a9eb949cac
MANUEL <iarrizabaloai23wg@ikzubirimanteo.com>
2023-11-02 11:24:04 +0100

    UD3/3/_6_ ariketa konponduta eta funtzionatzen du arazorik gabe

--- UD3 - Azpiprogramak eta datu-egiturak/3_Ariketa_Multzoa/_6_ariketa/src/Main.java
+++ UD3 - Azpiprogramak eta datu-egiturak/3_Ariketa_Multzoa/_6_ariketa/src/Main.java
@@ -22,24 +22,35 @@ public class Main {
         System.out.println("Sartu zenbakia txertatu nahi duzun posizioa (0-9):");
         int posizioa = sc.nextInt();
 
+
         for (int i = 0; i<zenbakiak.length;i++){
            if (i == posizioa){
                kopia[i]=zenb;
-               kopia[i+1] =zenbakiak[i];
-               System.out.println(zenbakiak[i]);
+               //kopia[i+1] =zenbakiak[i];
+               //System.out.println(zenbakiak[i]);
+               for (int e = i; e < zenbakiak.length-1; e++) {
+                   kopia[e+1] = zenbakiak[e];
+               }
+               i = zenbakiak.length;
+
            }else {
                kopia[i] = zenbakiak[i];
+
+
+
+
            }
         }
 
 
-        System.out.print(kopia[0]);
+        //imprimitzeko
+       // System.out.print(kopia[0]);
 
-        for (int i = 1;i< kopia.length;i++){
-            if (i < kopia.length/2-1){
-                System.out.print(" - "+kopia[i]+" - ");
+        for (int i = 0;i< kopia.length;i++){
+            if (i < kopia.length-1){
+                System.out.print(kopia[i]+" - ");
             }else {
-                System.out.print(" - "+kopia[i]);
+                System.out.print(kopia[i]);
 
             }
         }
