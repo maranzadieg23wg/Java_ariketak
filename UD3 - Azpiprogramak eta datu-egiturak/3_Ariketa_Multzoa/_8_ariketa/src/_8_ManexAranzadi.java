7ce1590ba770b204675d000b3fe29eebdc5ef4cc
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2023-11-13 19:07:04 +0100

    _2_ariketa konponduta, detektatzen du, ea simetrikoa den a la ez

---
+++ UD3 - Azpiprogramak eta datu-egiturak/3_Ariketa_Multzoa/_8_ariketa/src/_8_ManexAranzadi.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int [] zenbakiak = new int[10];
+        int [] kopia = new int[10];
+
+
+
+
+        //System.out.println("Sartu 9 zenbaki oso 10 elementuko array batean:");
+
+        for (int i = 0;i<zenbakiak.length;i++){
+            int zenb = (int)(Math.random()*20+1);
+            zenbakiak [i] = zenb;
+            kopia [i] =zenb;
+        }
+
+        System.out.println("1etik 20 arteko zenbaki ausazkoen array-a:");
+        for (int i = 0;i< zenbakiak.length;i++){
+            if (i < zenbakiak.length-1){
+                System.out.print(zenbakiak[i]+" - ");
+            }else {
+                System.out.println(zenbakiak[i]);
+
+            }
+        }
+        System.out.println("Zenbat aldiz mugitu egingo dira zenbakiak: ");
+        int zenbat = sc.nextInt();
+        zenbat--;
+
+       //int azkena = 0;
+
+        //zenbakiak mugitu eskubira
+        for (int i = 0;i<zenbat+1;i++){
+            for (int e = 0; e < zenbakiak.length-1; e++) {
+               //System.out.print("AZKENA: "+azkena+" - ");
+
+                //lehenengo zenbakia
+                kopia[0]=kopia[zenbakiak.length-1];
+
+                //Beste zenbakiak jartzen ditu.
+                kopia[e+1] = zenbakiak[e];
+            }
+
+            for (int e = 0; e < zenbakiak.length-1; e++) {
+                zenbakiak[e]=kopia[e];
+            }
+        }
+
+
+        System.out.println("Hau da bukaerako array-a:");
+        for (int i = 0;i< kopia.length;i++){
+            if (i < kopia.length-1){
+                System.out.print(kopia[i]+" - ");
+            }else {
+                System.out.print(kopia[i]);
+
+            }
+        }
+
+
+
+        sc.close();
+
+    }
+}
--- UD3 - Azpiprogramak eta datu-egiturak/4_Ariketa_Multzoa/_2_ariketa/src/Main.java
+++ UD3 - Azpiprogramak eta datu-egiturak/4_Ariketa_Multzoa/_2_ariketa/src/Main.java
@@ -7,6 +7,7 @@ public class Main {
         int [][] matrizea = new int[4][4];
 
 
+        boolean simetriko = true;
 
 
         System.out.println("Bete 4x4 matrize bat zenbaki osoekin:");
@@ -20,6 +21,20 @@ public class Main {
 
         inprimatu(matrizea);
 
+        for (int i = 0;i< matrizea.length;i++){
+            for (int e = 0;e<matrizea.length;e++){
+                if (matrizea[i][e]!=matrizea[e][i]){
+                    simetriko = false;
+                }
+            }
+        }
+
+        if (simetriko){
+            System.out.println("Matrizea simetrikoa da");
+        }else {
+            System.out.println("Matrizea ez simetrikoa da");
+
+        }
 
         sc.close();
 
