5af507c23d6a068fe6c38d53f9281939cfcf1b72
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-11-16 11:22:51 +0100

    Objektuak gehituta
    Objetuak definituta daude eta esan diegu imprimatzea, baina ez du main aurkitzen.

--- UD3.1 - Objetuetara bideratutako programazioa (OBO)/1_Ariketa multzoa/_1_ariketa/src/ProduktuProba.java
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/1_Ariketa multzoa/_1_ariketa/src/ProduktuProba.java
@@ -1,6 +1,20 @@
 public class ProduktuProba {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+
+        //Hemen sortzen dugu produktua
+        Produktua p1 = new Produktua("Pikmin 4",59.99, 7);
+        Produktua p2 = new Produktua("Mario Wonder",60, 2);
+        Produktua p3 = new Produktua("Metroid",50, 15);
+        Produktua p4 = new Produktua("Zelda Breath Of The Wild",60, 6);
+
+
+        //Objetuan dauden metodoak deitzeko
+        p1.erakutsiProduktuak();
+
+
+
+
+
     }
 }
