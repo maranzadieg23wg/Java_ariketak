bd916bfc0279e2b67f42038c746796b8509c5fdd
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-11-16 11:13:14 +0100

    Produktuak klasea eginda
    Datuak sartzeko konstruskotorea sortuta, eta inprimatzeko VOID-a ere bai.

--- UD3.1 - Objetuetara bideratutako programazioa (OBO)/1_Ariketa multzoa/_1_ariketa/src/Produktua.java
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/1_Ariketa multzoa/_1_ariketa/src/Produktua.java
@@ -3,7 +3,22 @@ public class Produktua {
     String izena;
     double prezioa;
     int kopurua;
-    
 
+
+    //eraikitzailea da
+    public Produktua(String i, double p, int k){
+        //deitzen dugunean funtzioari izena balioan gehitzean i izenarekin deituko diogu beste fitxategitik.
+        //i deitzen dio aldagai lokalari, eta izena aldagai generalari
+        //Ezin dute izen berberak eduki.
+        //.this erabili behar da aldagai generarali deitzeko bi baloreek izen berbera badaukate
+        this.izena = i;
+        this.prezioa = p;
+        this.kopurua = k;
+    }
+
+    //imprimatzeko produktuen datuak
+    public void erakutsiProduktuak(){
+        System.out.println(izena + " - "+prezioa+"€ - "+kopurua);
+    }
 
 }
