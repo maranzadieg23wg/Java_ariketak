eb9b40dffa4e1237d3417458368adcbe2e8c6339
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-11-17 08:39:31 +0100

    Main eginda
    Gehitzen ditugu zenbakiak p1 objetuari eta ondoren, imprimatzen dugu zenbat zenbaki imprimatu diren eta ondoren zein den beraien media.

--- UD3.1 - Objetuetara bideratutako programazioa (OBO)/_2_ariketa/src/Main.java
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/_2_ariketa/src/Main.java
@@ -1,8 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-
+    //Estadistika klase bat sortzen
     Estatistika p1 = new Estatistika();
+
+    //Gehitu zenbaki bat klasera
+    p1.gehituZenbaki(1);
+    p1.gehituZenbaki(2);
+    p1.gehituZenbaki(3);
+    p1.gehituZenbaki(4);
+    p1.gehituZenbaki(5);
+
+    //Deitzen diogu zenbat Zenbaki funtzioari gehitzeko gehitu ditugun.
+    p1.zenbatZenbaki();
+
+    //Deitzen diogu funtzioari erakusteko zein da sartu diren zenbakien media.
+    p1.media();
+
 
 
     }
