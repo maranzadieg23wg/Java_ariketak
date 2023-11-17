1cc773b664bac11d6e4c8b5fbaa10a0428079d21
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-11-17 08:57:19 +0100

    Gehitu egin dira zirkuluan behar diten VOIDak
    Gehitu egin dira kasi VOID guztiak, bakarrik falta berdinaDa funtzioa (Gehitu behar zaio beste objetu baten informazioa)

--- UD3.1 - Objetuetara bideratutako programazioa (OBO)/1_Ariketa multzoa/_3_ariketa/src/Zirkulua.java
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/1_Ariketa multzoa/_3_ariketa/src/Zirkulua.java
@@ -1,6 +1,40 @@
 public class Zirkulua {
 
-    
+    String kolorea;
 
+    int erradioa;
+
+    Zirkulua(String kolorea, int erradioa){
+
+        this.kolorea = kolorea;
+
+        this.erradioa = erradioa;
+    }
+
+    public void azalera(){
+        System.out.println("Azalera da: "+3.14151692*erradioa*erradioa);
+    }
+
+    public  void perimetroa(){
+        System.out.println("Perimetroa da: "+2*3.14151692*erradioa);
+    }
+
+    public void handiaDa(){
+        double azalera = 3.14151692*erradioa*erradioa;
+
+        if (azalera>20){
+            System.out.println("Zirkulua handia da.");
+        }else {
+            System.out.println("Zirkulua txikia da.");
+        }
+    }
+
+    public void berdinaDa(){}
+
+    public void erakutsi(){
+        System.out.println("Zirkuluaren kolorea da: "+kolorea);
+
+        System.out.println("Zirkuluaren erradioa da: "+erradioa);
+    }
 
 }
