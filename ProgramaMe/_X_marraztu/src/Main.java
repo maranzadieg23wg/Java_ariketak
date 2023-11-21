7bc49765c432066fe6e52e529205c9a4804783a1
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2023-11-21 18:46:51 +0100

    Egiten
    X marrazten du, baina egin behar dut azpiprograma bat ikusteko ea sartutako zenbakiak balio duen, baina hori egitean esaten dit dagoen zenbakia ez dela esistitzen.

--- ProgramaMe/_X_marraztu/src/Main.java
+++ ProgramaMe/_X_marraztu/src/Main.java
@@ -1,17 +1,28 @@
+import java.util.Scanner; //Importatu objetua
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Sartu zenbatekoa izan behar den");
+
+        Scanner sc = new Scanner(System.in);
+        boolean balioDu = false;
+
+        while (!balioDu){
+            int zenbat = sc.nextInt();
+            balioDu =zenbakiaEskatu(zenbat);
+        }
 
 
-        int zenbat = 7;
+
 
         for (int i =0;i<zenbat;i++){
             for (int e =0;e<zenbat;e++){
-               // System.out.print(i+e + " ");
+                //System.out.print(i+e + " ");
 
-                if (i+e == zenbat-1 || i ==0 || i == zenbat-1){
+                if (i+e == zenbat-1 || i-e == 0 || i ==0 || i == zenbat-1){
                     System.out.print("* ");
+                }else {
+                    System.out.print("  ");
                 }
             }
             System.out.println(" ");
@@ -19,4 +30,20 @@ public class Main {
 
 
     }
+
+
+    public static boolean zenbakiaEskatu(int zenbat){
+
+        if (zenbat <4 || zenbat %2 !=0){
+            System.out.println("ERROR: Sartutako zenbakiak ez du balio.");
+            return false;
+        }else {
+            return true;
+        }
+
+
+
+
+    }
+
 }
