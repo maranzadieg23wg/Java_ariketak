655e9c824bf5a8fac56158f8e9a2c50ba504fb8b
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2023-11-21 19:00:41 +0100

    _X_Eginda eta bukatuta
    Konpondu egin dut eta ongi funtzionatzen du guztia.

--- ProgramaMe/_X_marraztu/src/Main.java
+++ ProgramaMe/_X_marraztu/src/Main.java
@@ -5,30 +5,38 @@ public class Main {
         System.out.println("Sartu zenbatekoa izan behar den");
 
         Scanner sc = new Scanner(System.in);
-        boolean balioDu = false;
+        /*boolean balioDu = false;
 
         while (!balioDu){
-            int zenbat = sc.nextInt();
-            balioDu =zenbakiaEskatu(zenbat);
+            //int zenbat = sc.nextInt();
+            //balioDu =zenbakiaEskatu(zenbat);
+        }*/
+
+
+        int zenbat = sc.nextInt();
+
+        if (zenbat <4 || zenbat %2 ==0){
+            System.out.println("ERROR: Sartutako zenbakiak ez du balio.");
+            zenbat =0;
         }
 
 
 
 
         for (int i =0;i<zenbat;i++){
-            for (int e =0;e<zenbat;e++){
-                //System.out.print(i+e + " ");
+                for (int e =0;e<zenbat;e++){
+                    //System.out.print(i+e + "  ");
 
-                if (i+e == zenbat-1 || i-e == 0 || i ==0 || i == zenbat-1){
-                    System.out.print("* ");
-                }else {
-                    System.out.print("  ");
+                    if (i+e == zenbat-1 || i-e == 0 || i ==0 || i == zenbat-1){
+                        System.out.print("* ");
+                    }else {
+                        System.out.print("  ");
+                    }
                 }
-            }
-            System.out.println(" ");
-        }
+        System.out.println(" ");
 
 
+            sc.close();
     }
 
 
