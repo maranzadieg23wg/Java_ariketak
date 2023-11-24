822c09e2849a40643fc0335d6cd445ec349c0acd
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-11-24 08:20:46 +0100

    Liburutegia sortuta eta bikoitia detekatzen du
    Sortu egin da liburutegia klasea eta honekin batera funtzio bat detekatzeko ea bikoitia den a la ez.

---
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/1_Ariketa multzoa/_4_ariketa/src/Liburutegia.java
@@ -0,0 +1,38 @@
+public class Liburutegia {
+
+
+
+    //int zenbakia;
+
+
+
+    Liburutegia(int zenbakia){
+        //this.zenbakia = zenbakia;
+    }
+
+
+    public void bikoitiaDa(int zenbakia){
+
+        boolean bikoiti = false;
+
+        if (zenbakia%2 ==0){
+            bikoiti=true;
+        }
+
+        if (bikoiti){
+            System.out.println("Sartutako zenbakia bikoitia da.");
+        }else {
+            System.out.println("Sartutako zenbakia ez bikoitia da.");
+        }
+
+
+    }
+
+    public void digituKopurua(int zenbakia){
+        
+    }
+
+
+
+
+}
