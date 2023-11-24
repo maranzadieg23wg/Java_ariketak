b10eceb486c31d3db5f0defc2487d794a90ea77a
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-11-24 08:43:55 +0100

    _Liburutegia_ konponduta
    Void kenduta bueltatzeko informazioa, eta ez  du simplemente imprimatzen erantzuna.

--- UD3.1 - Objetuetara bideratutako programazioa (OBO)/1_Ariketa multzoa/_4_ariketa/src/Liburutegia.java
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/1_Ariketa multzoa/_4_ariketa/src/Liburutegia.java
@@ -11,44 +11,46 @@ public class Liburutegia {
     }
 
 
-    public void bikoitiaDa(int zenbakia){
+    public static boolean bikoitiaDa(int zenbakia){
 
-        boolean bikoiti = zenbakia % 2 == 0;
+        return zenbakia % 2 == 0;
 
-        if (bikoiti){
+        /*if (bikoiti){
             System.out.println("Sartutako zenbakia bikoitia da.");
         }else {
             System.out.println("Sartutako zenbakia ez bikoitia da.");
-        }
+        }*/
 
 
     }
 
-    public void digituKopurua(int zenbakia){
-        int zenbat = (int)(Math.log10(zenbakia)+1);
-        System.out.println(zenbat+" digitu ditu zenbaki honek.");
+    public static int digituKopurua(int zenbakia){
+        return (int)(Math.log10(zenbakia)+1);
+        //System.out.println(zenbat+" digitu ditu zenbaki honek.");
     }
 
-    public void bostDitu(int zenbakia){
+    public static boolean bostDitu(int zenbakia){
         int zenbat = (int)(Math.log10(zenbakia)+1);
 
         if (zenbat>=5){
-            System.out.println("Zenbaki honek 5 digitu baino gehiago ditu.");
+            //System.out.println("Zenbaki honek 5 digitu baino gehiago ditu.");
+            return true;
 
         }else {
-            System.out.println("Zenbaki honek ez ditu 5 digitu baino gehiago.");
+            //System.out.println("Zenbaki honek ez ditu 5 digitu baino gehiago.");
+            return false;
         }
 
     }
 
-    public void hamarrenMultiploa(int zenbakia){
-        boolean hamartar = zenbakia % 10 == 0;
+    public static boolean hamarrenMultiploa(int zenbakia){
+        return zenbakia % 10 == 0;
 
-        if (hamartar){
+        /*if (hamartar){
             System.out.println("Sartutako zenbakia hamarren multiploa da.");
         }else {
             System.out.println("Sartutako zenbakia ez da hamarren multiploa.");
-        }
+        }*/
     }
 
 
