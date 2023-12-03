fa3a18d3b613e1353dce06afc4536c96de9d7b86
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-12-03 12:10:56 +0100

    GehituBidaia gehituta
    Gehitu egin det gehitu bidaia. Kendu egin ditut zeuden komentarioak main fitxategian eta puntuSisteman gehitu egin ditut espazio gehiago.

--- Erronka/Renfe/src/Erabiltzaileak.java
+++ Erronka/Renfe/src/Erabiltzaileak.java
@@ -1,19 +1,25 @@
+import java.util.List;
+
 public class Erabiltzaileak {
 
     String DNI;
     String izena;
     String abizena;
-    int bidaiKopurua;
     int puntuak;
+    List<Object> bidaiak;
 
 
-    Erabiltzaileak(String DNI, String izena, String abizena, int bidaiKopurua, int puntuak){
+
+    Erabiltzaileak(String DNI, String izena, String abizena, int puntuak){
         this.DNI = DNI;
         this.izena=izena;
         this.abizena=abizena;
-        this.bidaiKopurua = bidaiKopurua;
         this.puntuak = puntuak;
     }
+
+    public void gehituBidaiBai(int idBidaia, int estimazioDebora)
+
+
 
 
 
--- Erronka/Renfe/src/Main.java
+++ Erronka/Renfe/src/Main.java
@@ -1,16 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        // Crear un objeto Ikastaroak
+
         Ikastaroak ikastaroak = new Ikastaroak();
 
-        // Crear 5 ejemplos de empleados (Langile) con apellidos vascos
         Langile langile1 = new Langile("11111111A", "Manex", "Etxeberria", 123456789, 987654321, "manex@example.com", 1990);
         Langile langile2 = new Langile("22222222B", "Leire", "Zubizarreta", 234567890, 876543210, "leire@example.com", 1985);
         Langile langile3 = new Langile("33333333C", "Janire", "Urrutia", 345678901, 765432109, "janire@example.com", 1988);
         Langile langile4 = new Langile("44444444D", "Ane", "Gonzalez", 456789012, 654321098, "ane@example.com", 1995);
         Langile langile5 = new Langile("55555555E", "Iker", "Larrinaga", 567890123, 543210987, "iker@example.com", 1983);
 
-        // Añadir cursos a los empleados con los nuevos nombres de curso y estados
         langile1.gehituIkastaroa(1, "ikastaro1", 2023, 2024, "ganditua");
         langile1.gehituIkastaroa(2, "ikastaro2", 2023, 2024, "ez gainditua");
         langile2.gehituIkastaroa(2, "ikastaro2", 2023, 2024, "ez gainditua");
@@ -18,7 +16,6 @@ public class Main {
         langile4.gehituIkastaroa(1, "ikastaro1", 2023, 2024, "ganditua");
         langile5.gehituIkastaroa(4, "ikastaro4", 2023, 2024, "egiten");
 
-        // Mostrar información de empleados y sus cursos
         langile1.datuakErakutsi();
         langile1.ikusiIkastaroak();
         System.out.println("-----------------------------------");
@@ -34,7 +31,6 @@ public class Main {
         langile5.datuakErakutsi();
         langile5.ikusiIkastaroak();
 
-        // Cambiar datos de un empleado y añadir/borrar cursos
         langile1.datuakAldatu("11111111B", "Manexa", "Etxeberria", 123456789, 987654321, "manex@example.com", 1990);
         langile1.gehituIkastaroa(5, "ikastaro5", 2023, 2024, "egiten");
         langile1.kenduIkastaroa(1);
