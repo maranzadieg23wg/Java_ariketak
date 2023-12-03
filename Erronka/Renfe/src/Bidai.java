af1d0b8b7b0dd35620ef2f61fd16b3a481f21740
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-12-03 11:57:02 +0100

    Erabiltzaile eta bidai konstruktorea gehituta
    Gehitu egin ditut behar dituen konstruktoreak erabiltzaile eta bidaia objetuek.

--- Erronka/Renfe/src/Bidai.java
+++ Erronka/Renfe/src/Bidai.java
@@ -1,2 +1,19 @@
 public class Bidai {
+
+
+    int idBidaia;
+    int estimazioDenbora;
+    int okupazioa;
+
+    int balorazioa;
+
+    String helmuga;
+
+    Bidai(String helmuga, int estimazioDenbora){
+        this.helmuga=helmuga;
+        this.estimazioDenbora=estimazioDenbora;
+    }
+
+
+
 }
--- Erronka/Renfe/src/Erabiltzaileak.java
+++ Erronka/Renfe/src/Erabiltzaileak.java
@@ -1,2 +1,21 @@
 public class Erabiltzaileak {
+
+    String DNI;
+    String izena;
+    String abizena;
+    int bidaiKopurua;
+    int puntuak;
+
+
+    Erabiltzaileak(String DNI, String izena, String abizena, int bidaiKopurua, int puntuak){
+        this.DNI = DNI;
+        this.izena=izena;
+        this.abizena=abizena;
+        this.bidaiKopurua = bidaiKopurua;
+        this.puntuak = puntuak;
+    }
+
+
+
+
 }
