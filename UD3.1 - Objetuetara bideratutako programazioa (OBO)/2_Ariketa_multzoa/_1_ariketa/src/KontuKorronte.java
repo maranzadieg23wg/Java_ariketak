135f014d8079dc22d96d2c0747ed8831533f5320
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-12-11 09:44:28 +0100

    Bi konstruktore sortuta
    Sortu egin ditut behar dituen bi konstruktoreak konu korronte programan.

---
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/2_Ariketa_multzoa/_1_ariketa/src/KontuKorronte.java
@@ -0,0 +1,20 @@
+public class KontuKorronte {
+
+
+    String izena;
+    float dirua;
+
+    KontuKorronte (String izena){
+        this.izena=izena;
+
+        this.dirua=0;
+    }
+
+    KontuKorronte(String izena, float dirua){
+        this.izena=izena;
+
+        this.dirua=dirua;
+    }
+
+
+}
