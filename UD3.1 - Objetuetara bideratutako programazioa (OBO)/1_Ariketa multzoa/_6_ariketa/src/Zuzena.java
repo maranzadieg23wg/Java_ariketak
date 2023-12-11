66780a4672c5909f32364141d7837b3a5d63187b
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-12-11 08:22:43 +0100

    Bigarren puntua gehituta

--- UD3.1 - Objetuetara bideratutako programazioa (OBO)/1_Ariketa multzoa/_6_ariketa/src/Zuzena.java
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/1_Ariketa multzoa/_6_ariketa/src/Zuzena.java
@@ -7,17 +7,28 @@ public class Zuzena {
     int zX;
     int zY;
 
+    int zX2;
+    int zY2;
+
 
     Zuzena(float zabalera, Puntua puntua){
 
         this.zabalera = zabalera;
 
+        //Lehenengo puntua
         this.zX = puntua.getzX();
         this.zY=puntua.getzY();
 
+        //Bigarren puntua
+        this.zX2 = puntua.getzX();
+        this.zY2=puntua.getzY();
 
     }
-    
+
+    public boolean horizontalaDa(){
+
+    }
+
 
 
 
