d4f16763a55689249ff2fd5774e58b6cf15dc45b
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-12-11 08:32:27 +0100

    Gelditzen diren funtzioak gehituta
    Gehitu egin ditut gelditzen ziren objetuak.

--- UD3.1 - Objetuetara bideratutako programazioa (OBO)/1_Ariketa multzoa/_6_ariketa/src/Zuzena.java
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/1_Ariketa multzoa/_6_ariketa/src/Zuzena.java
@@ -26,7 +26,54 @@ public class Zuzena {
     }
 
     public boolean horizontalaDa(){
+        boolean horizontala=false;
 
+        if (zX == zX2){
+            horizontala =true;
+        }
+
+        return horizontala;
+    }
+
+    public boolean bertikalaDa(){
+        boolean bertikala=false;
+
+        if (zY == zY2){
+            bertikala =true;
+        }
+
+        return bertikala;
+    }
+
+    public int altuena(){
+
+        if (zY>zY2){
+            return zY;
+        }else {
+            return  zY2;
+        }
+
+
+    }
+
+    public float getLuzera(){
+        return zabalera;
+    }
+
+    public float luzeena(Zuzena zuzena){
+        float luzeera2 = getLuzera();
+
+        if (this.zabalera > luzeera2){
+            return this.zabalera;
+        }else {
+            return luzeera2;
+        }
+    }
+
+    public void erakutsiZuzena(){
+        System.out.println("X: "+zX);
+        System.out.println("Y: "+zY);
+        System.out.println("Luzeera: "+zabalera);
     }
 
 
