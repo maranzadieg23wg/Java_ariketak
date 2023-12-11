b01896498b2103f02b214190a53b9fdee07b86f4
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-12-11 09:01:32 +0100

    Gehituta proba datu guztiak

--- UD3.1 - Objetuetara bideratutako programazioa (OBO)/1_Ariketa multzoa/_6_ariketa/src/ZuzenaProba.java
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/1_Ariketa multzoa/_6_ariketa/src/ZuzenaProba.java
@@ -5,16 +5,32 @@ public class ZuzenaProba {
         Puntua p1 = new Puntua(1,2);
         Puntua p2 = new Puntua(-1, 2);
 
+        Puntua p3 = new Puntua(1,-2);
+        Puntua p4 = new Puntua(-1, -2);
+
         //Lehenengo zuzena sortzen
         Zuzena z1 = new Zuzena(1, p1);
         //Zuzenari gehitzen bigarren puntu bat
         z1.bigarrenPuntua(p2);
+
+        //Bigarren zuzena sortzen
+        Zuzena z2 = new Zuzena(1, p3);
+        //Zuzenari gehitzen bigarren puntu bat
+        z2.bigarrenPuntua(p4);
 
 
         System.out.println(z1.horizontalaDa());
         System.out.println(z1.bertikalaDa());
 
         System.out.println(z1.altuena());
+
+        System.out.println(z1.getLuzera());
+
+        System.out.println(z1.luzeena(z2));
+
+
+        z1.erakutsiZuzena();
+
 
 
     }
