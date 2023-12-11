155d802e1a3fe47e1e40650fce4a2ebe2eae551a
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-12-11 09:53:54 +0100

    Pertsona klasea gehituta
    Sortu egin det pertsona klasea eta ondoren gehitu egin dut eraikitzailea eta behar diren funtzioak lortzeko datuak.

--- UD3.1 - Objetuetara bideratutako programazioa (OBO)/2_Ariketa_multzoa/_1_ariketa/src/Pertsona.java
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/2_Ariketa_multzoa/_1_ariketa/src/Pertsona.java
@@ -1,6 +1,26 @@
 public class Pertsona {
 
-    
+    String izena;
+    String DNI;
+    int adina;
+
+
+    Pertsona(String izena, String DNI, int adina){
+        this.izena=izena;
+        this.DNI=DNI;
+        this.adina=adina;
+    }
+
+    public String getIzena(){
+        return this.izena;
+    }
+    public String getDNI(){
+        return this.DNI;
+    }
+    public int getAdina(){
+        return this.adina;
+    }
+
 
 
 }
