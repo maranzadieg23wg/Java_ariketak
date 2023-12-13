d3e9c39da480c681cc7b2a299d24bf2863a6280d
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-12-13 10:01:19 +0100

    Datu proba gehituta
    Probatzen, gehitu dira datu probak, ea funtzionatzen duen.

--- UD3.1 - Objetuetara bideratutako programazioa (OBO)/2_Ariketa_multzoa/_1_ariketa/src/Main.java
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/2_Ariketa_multzoa/_1_ariketa/src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Pertsona p1 = new Pertsona("Manolo", "12345678A", 20);
+
+        KontuKorronte k1 = new KontuKorronte(p1);
+
+        Banku b1 = new Banku("Kutxabank");
+
+
+        b1.gehituKontuKorronte(k1);
+
+        //k1.erakutsiKontua();
+
+        k1.diruGordailua(60);
+        k1.diruaKendu(120);
+
+        b1.kenduKontuKorronte(k1);
+        b1.ikusiKontuKorronte(k1);
+
     }
 }
