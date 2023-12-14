f733954fd46d3e4caf81edd59ad2f5a99fb1442d
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-12-14 10:57:07 +0100

    Jokoa objetua bukatuta
    Gehitu egin dira behar dituen funtzio guztiak funtzionatzeko

--- UD3.1 - Objetuetara bideratutako programazioa (OBO)/3_ariketa_multzoa/_2_ariketa/src/Jokoa.java
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/3_ariketa_multzoa/_2_ariketa/src/Jokoa.java
@@ -1,2 +1,28 @@
 public class Jokoa {
+
+    private Object[] jokalariak = new Object[10];
+    Jokoa(){
+
+    }
+
+    public void gehituJokalaria(Jokalari jokalari){
+        for (int i =0;i<jokalariak.length;i++){
+            if (jokalariak[i]!=null){
+                jokalariak[i]=jokalari;
+                i= jokalariak.length+5;
+            }
+        }
+    }
+
+    public int jokalariKop(){
+        for (int i =0;i<jokalariak.length;i++){
+            if (jokalariak[i]==null){
+                return i;
+            }
+
+        }
+        return 10;
+    }
+
+
 }
