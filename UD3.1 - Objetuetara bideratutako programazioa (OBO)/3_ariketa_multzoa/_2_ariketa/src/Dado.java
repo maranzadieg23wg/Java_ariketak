3cb246e282bcad2628778eba1c246d77ffbadf2c
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-12-14 10:45:41 +0100

    Eraikitzailea gehituta eta get funtzioa
    Gehitu egin dut eraikitzailea eta honekin batera azkeneko zenbakia lortzeko funtzioak

--- UD3.1 - Objetuetara bideratutako programazioa (OBO)/3_ariketa_multzoa/_2_ariketa/src/Dado.java
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/3_ariketa_multzoa/_2_ariketa/src/Dado.java
@@ -4,10 +4,17 @@ public class Dado {
     private int azkZenbakia = 0;
     private int jaurtiketaKop = 0;
 
+    Dado(){
+
+    }
 
     public void jaurti(){
         this.azkZenbakia=(int) (Math.random()*6+1);
         jaurtiketaKop++;
+    }
+
+    public int getAzkZenbakia(){
+        return azkZenbakia;
     }
 
     public void erakutsi(){
