d41cc38a795276b4b690b64add7ed008fd02ae74
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-12-14 10:46:43 +0100

    Jokalari eraikitzaile eta funtzioak gehituta
    Gehitu egin da eraikitzailea eta honekin behar duen funtzioa ikusteko dadoa

--- UD3.1 - Objetuetara bideratutako programazioa (OBO)/3_ariketa_multzoa/_2_ariketa/src/Jokalari.java
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/3_ariketa_multzoa/_2_ariketa/src/Jokalari.java
@@ -1,2 +1,21 @@
 public class Jokalari {
+
+    private String izena;
+    private  Dado d1;
+
+
+    public Jokalari(String izena){
+        this.izena=izena;
+        this.d1 =new Dado();
+    }
+
+    public void jaurti() {
+        this.d1.jaurti();
+    }
+
+    public void jasoBalioa(){
+        System.out.println("Zenbakia: "+this.d1.getAzkZenbakia());
+    }
+
+
 }
