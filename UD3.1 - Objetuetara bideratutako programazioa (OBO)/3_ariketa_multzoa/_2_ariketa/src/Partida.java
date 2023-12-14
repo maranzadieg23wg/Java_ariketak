ca3f8bb84464063d224bcf77ecb9ffdeae473728
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-12-14 11:00:40 +0100

    Izena aldatuta fitxategiari
    Izena aldatu diot fitxategiari.

---
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/3_ariketa_multzoa/_2_ariketa/src/Partida.java
@@ -0,0 +1,23 @@
+public class Partida {
+    public static void main(String[] args) {
+
+
+        Jokalari j1 = new Jokalari("Manolo");
+        Jokalari j2 = new Jokalari("Patxi");
+        Jokalari j3 = new Jokalari("Begoña");
+
+        for (int i =0;i<6;i++){
+            j1.jaurti();
+            j2.jaurti();
+            j3.jaurti();
+        }
+
+        
+
+
+
+
+
+
+    }
+}
