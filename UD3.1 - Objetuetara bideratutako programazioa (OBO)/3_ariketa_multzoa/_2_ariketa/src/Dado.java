8a407dcc8e5c148b6becd5d1956b14cd98ad7282
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-12-14 10:27:55 +0100

    Dado objetuak behar dituen atributu eta funtzioak gehituta
    Gehitu zaizkio behar dituen atributuak eta honekin batera, behar dituen funtzioak, jaurti eta erakutsi dadoaren datuak.

--- UD3.1 - Objetuetara bideratutako programazioa (OBO)/3_ariketa_multzoa/_2_ariketa/src/Dado.java
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/3_ariketa_multzoa/_2_ariketa/src/Dado.java
@@ -1,2 +1,21 @@
 public class Dado {
+
+
+    private int azkZenbakia = 0;
+    private int jaurtiketaKop = 0;
+
+
+    public void jaurti(){
+        this.azkZenbakia=(int) (Math.random()*6+1);
+        jaurtiketaKop++;
+    }
+
+    public void erakutsi(){
+        System.out.println("--------------------------------");
+        System.out.println("Azkeneko zenbakia: "+azkZenbakia);
+        System.out.println("Jaurtiketa kopurua: "+jaurtiketaKop);
+        System.out.println("--------------------------------");
+
+    }
+
 }
