feaa1e190dbbd4c1de050d6d3aef9322535a8f74
Manex <maranzadieg23wg@ikzubirimanteo.com>
2023-12-15 08:48:41 +0100

    Palindromo ariketa eginda
    Bukatu egin det palindromoen ariketa eta guztiak ongi funtzionatzen du.

--- UD3.1 - Objetuetara bideratutako programazioa (OBO)/2_Ariketa_multzoa/_2_ariketa/src/Palindromo.java
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/2_Ariketa_multzoa/_2_ariketa/src/Palindromo.java
@@ -1,4 +1,68 @@
 public class Palindromo {
 
-    
+
+
+
+    long zenbaki;
+
+    Palindromo(long zenbaki){
+        this.zenbaki=zenbaki;
+    }
+
+    Palindromo(){
+        this.zenbaki=0;
+    }
+
+
+    public boolean palindromoaDa(){
+
+        boolean palindromo = false;
+
+        char [] lista = new char[digituKop()];
+
+        for (int i =0;i<digituKop()/2;i++){
+            lista[i]= digiPosN(i);
+        }
+        //Ikusteko nola dagoen lista
+        /*for (int i =0;i<digituKop()/2;i++){
+            System.out.print(lista[i]+" ");
+        }*/
+
+        for (int i =0;i<digituKop();i++){
+            if (digiPosN(i) !=digiPosN(digituKop()-i-1)){
+                palindromo = false;
+                i=digituKop()+1;
+            }else {
+                palindromo = true;
+            }
+        }
+
+        return palindromo;
+
+    }
+
+
+    public int digituKop(){
+        return (int)(Math.log10(this.zenbaki)+1);
+    }
+
+    public int digituKop(int zenbaki){
+        return (int)(Math.log10(zenbaki)+1);
+    }
+
+
+
+    public char digiPosN(int n){
+
+        String s = String.valueOf(this.zenbaki);
+        return s.charAt(n);
+    }
+
+    public int digiPosN(long zenbaki,int n){
+
+        String s = String.valueOf(zenbaki);
+        return s.charAt(n);
+    }
+
+
 }
---
+++ UD3.1 - Objetuetara bideratutako programazioa (OBO)/2_Ariketa_multzoa/_2_ariketa/src/PalindromoProba.java
@@ -0,0 +1,18 @@
+public class PalindromoProba {
+    public static void main(String[] args) {
+
+
+
+        Palindromo p1 = new Palindromo(345543);
+
+        System.out.println(p1.digiPosN(0));
+        System.out.println(p1.digituKop());
+        System.out.println(p1.palindromoaDa());
+
+
+
+
+
+
+    }
+}
