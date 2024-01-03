483c8d08a0044f16f3de5d2f9c25c21182427ac5
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-01-03 13:55:43 +0100

    Bezero klasea sortuta
    Sortu egin da bezero klasea eta gehitu egin dira behar diren funtzioak. Probatu egin da funtzionatzen duela.

--- UD4 - Objetuetara bideratutako programazioa (OBO)/4_ariketak_Gabonak/_Gabonetako_ariketak/src/Bezero.java
+++ UD4 - Objetuetara bideratutako programazioa (OBO)/4_ariketak_Gabonak/_Gabonetako_ariketak/src/Bezero.java
@@ -1,2 +1,52 @@
 public class Bezero {
+
+    private String izena;
+    private String NAN;
+    private boolean bazkide;
+    private String maila;
+
+    public Bezero (String izena, String NAN){
+        this.izena = izena;
+        this.NAN = NAN;
+        this.bazkide = false;
+    }
+
+    public Bezero(String izena, String NAN, String maila){
+        this.izena = izena;
+        this.NAN = NAN;
+        this.bazkide = true;
+        this.maila = maila;
+    }
+
+    public double jasoDeskontua(float zenbat){
+
+        switch (this.maila){
+            case "Premium":
+                return (zenbat *0.2);
+                //break;
+
+            case "Urre":
+                return (zenbat *0.15);
+                //break;
+
+            case "Zilar":
+                return (zenbat *0.2);
+                //break;
+
+            default:
+                return zenbat;
+                //break;;
+        }
+
+    }
+
+    public String toString(){
+        if (!bazkide){
+            return "Izena: "+ izena+" -- NAN: "+NAN;
+        }else {
+            return "Izena: "+ izena+" -- NAN: "+NAN+" -- Bazkide: "+bazkide+" -- Maila: "+maila;
+        }
+
+    }
+
 }
--- UD4 - Objetuetara bideratutako programazioa (OBO)/4_ariketak_Gabonak/_Gabonetako_ariketak/src/Main.java
+++ UD4 - Objetuetara bideratutako programazioa (OBO)/4_ariketak_Gabonak/_Gabonetako_ariketak/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        Bezero b1 = new Bezero("Manex", "12345678R");
+        Bezero b2 = new Bezero("Manex", "12345678R", "Urre");
+
+
+        System.out.println(b1.toString());
+        System.out.println(b2.toString());
+
 
     }
 }
