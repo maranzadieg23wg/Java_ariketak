f29a6161075063782d4dcc48a8375907338af94a
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-01-03 20:09:17 +0100

    Apaindegia klasea geitzen funtzioak
    Gehitu egin dira behar dituen funtzio batzuk, falta dira horaindik gehitzea bisitena, baita poliki poliki forma artzen doa.

--- UD4 - Objetuetara bideratutako programazioa (OBO)/4_ariketak_Gabonak/_Gabonetako_ariketak/src/Apaindegi.java
+++ UD4 - Objetuetara bideratutako programazioa (OBO)/4_ariketak_Gabonak/_Gabonetako_ariketak/src/Apaindegi.java
@@ -1,2 +1,60 @@
 public class Apaindegi {
+
+    private int luzeera;
+    private Object bezeroList []= new Object [luzeera];
+    private Object bisitaList []= new Object [luzeera];
+
+    private int bezeroKant;
+    private int bisitaKant;
+
+
+    Apaindegi(int luzeera){
+
+        this.luzeera = luzeera;
+        bezeroKant =0;
+        bisitaKant =0;
+    }
+
+    public void bajaEman(Bezero bezero){
+        bezero.setBazkide(false);
+    }
+
+    public void esleituBazkideMaila(Bezero bezero, String maila){
+        if (maila.equals("Premium") || maila.equals("Urre")|| maila.equals("Zilar")){
+            bezero.setMaila(maila);
+        }else {
+            System.out.println("[-] Sartutako maila ez da ezistitzen");
+            System.out.println("[-] Premium -- Urre -- Zilar");
+        }
+    }
+
+    public Bezero jasoBezeroa(String NAN){
+        for (int i =0;i<bezeroList.length;i++){
+            Bezero bezero = (Bezero) bezeroList[i];
+            if (NAN.equals(bezero.getNAN())){
+                i = bezeroList.length +2;
+                return (Bezero) bezeroList[i];
+            }
+        }
+
+        return null;
+        //return "Ez da aurkitu "+NAN+" NANa daukagun bezeroa.";
+    }
+
+    public void gehituBezeroa(Bezero bezero){
+        bezeroList[bezeroKant]= bezero;
+        bezeroKant++;
+    }
+
+    public void gehituBisita(String NAN, java.time.LocalDate data){
+        Bisita bisita = new Bisita(jasoBezeroa(NAN), data);
+        bisitaList[bisitaKant]= bisita;
+        bisitaKant++;
+    }
+
+    
+
+
+
+
 }
