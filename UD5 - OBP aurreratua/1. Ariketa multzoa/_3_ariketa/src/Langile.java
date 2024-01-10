14c5414c2c8890ab34d666466372e1a79e97d2ac
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-01-10 09:07:57 +0100

    Langile klasea sortuta
    Sortu egin da langile klasea eta honek behar duen eraikitzailea

---
+++ UD5 - OBP aurreratua/1. Ariketa multzoa/_3_ariketa/src/Langile.java
@@ -0,0 +1,19 @@
+public class Langile {
+
+    private String izena;
+    private String abizena;
+    private int gsz;
+
+    public Langile(String izena, String abizena, int gsz){
+        this.izena = izena;
+        this.abizena=abizena;
+        this.gsz=gsz;
+    }
+
+    public String toString(){
+        return "Izena: "+izena+" Abizena:"+abizena+ "GSZ: "+gsz;
+    }
+
+
+
+}
