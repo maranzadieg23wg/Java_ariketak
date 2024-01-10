bac82c848f686e7b7e81750e18bbeaa797cf5869
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-01-10 09:36:03 +0100

    SoldatapekoKomisioLangile objetua sortuta
    Sortu egin da soldatapeko komisio langile objetua eta honek behar dituen funtzioak gehituta.

---
+++ UD5 - OBP aurreratua/1. Ariketa multzoa/_3_ariketa/src/SoldatapekoKomisioLangile.java
@@ -0,0 +1,23 @@
+public class SoldatapekoKomisioLangile extends KomisioLangile{
+
+
+    float oinarrizkoSoldata;
+
+    SoldatapekoKomisioLangile(String izena, String abizena, int sgz, float salmenta,float komisioa, float oinarrizkoSoldata){
+        super(izena, abizena,sgz,salmenta,komisioa);
+        this.oinarrizkoSoldata = oinarrizkoSoldata;
+        if (oinarrizkoSoldata<0){
+            oinarrizkoSoldata =0;
+        }
+    }
+
+    public float irabaziak(){
+        return oinarrizkoSoldata+salmentak*komisioa;
+    }
+
+    public String toString(){
+        return super.toString()+" Oinarrizko Soldata: "+oinarrizkoSoldata;
+    }
+
+
+}
