e8ab3ec117ecf1822b5b0726fc2f37df391eee11
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-01-10 09:53:12 +0100

    Gehitu egin da 5 maileguen limitea
    Limitatu egin da mailegu kopurua 5 unitatetara.

--- UD5 - OBP aurreratua/1. Ariketa multzoa/_02_ariketa/src/eskaerak/Bezero.java
+++ UD5 - OBP aurreratua/1. Ariketa multzoa/_02_ariketa/src/eskaerak/Bezero.java
@@ -6,14 +6,41 @@ public class Bezero {
     String izena;
     int maileguKop;
 
-    Bezero(String NAN, String izena, int maileguKop){
+    private Mailegu maileguList[];
+
+    Bezero(String NAN, String izena){
         this.NAN = NAN;
         this.izena = izena;
-        this.maileguKop = maileguKop;
+        maileguKop =0;
+        maileguList = new Mailegu[5];
     }
 
     public void erakutsi(){
         System.out.println("NAN: "+NAN+" Izena: "+izena+" Mailegu Kopurua: "+maileguKop);
+    }
+
+    public void gehituMailegu(Mailegu mailegu){
+        if (maileguKop < maileguList.length){
+            System.out.println("Bezeroak ditu maileguan "+maileguKop+" liburu, ezin ditu gehiago artu. Bueltatu behar du baten bat.");
+            mailegu = null;
+        }
+        for (int i =0;i<maileguList.length;i++){
+
+            if (maileguList[i]==null){
+                maileguList[i]=mailegu;
+                i = maileguList.length;
+                maileguKop++;
+            }
+
+        }
+    }
+
+    public void kenduMailegu(Mailegu mailegu){
+        for (int i =0;i<maileguList.length;i++){
+            if (maileguList[i].equals(mailegu)){
+                maileguList[i]=null;
+            }
+        }
     }
 
 }
