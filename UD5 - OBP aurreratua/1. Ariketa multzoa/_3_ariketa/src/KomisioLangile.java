0cedbf962959dd8f403e3af32229842a4c6c70db
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-01-10 09:27:54 +0100

    Komisio langile objetua sortuta
    Sortu egin da komisio langile objetua eta honek behar dituen klaseak eta funtzioak.

---
+++ UD5 - OBP aurreratua/1. Ariketa multzoa/_3_ariketa/src/KomisioLangile.java
@@ -0,0 +1,24 @@
+public class KomisioLangile extends Langile{
+    float salmentak;
+    float komisioa;
+
+
+    KomisioLangile(String izena, String abizena, int gsz, float salmentak, float komisioa){
+        super(izena, abizena, gsz);
+        this.salmentak = salmentak;
+        this.komisioa = komisioa;
+
+        if (this.salmentak <0){
+            salmentak =0;
+        }
+        if (this.komisioa >0 && this.komisioa < 1){
+            this.komisioa =0;
+        }
+    }
+
+    public float irabaziak(){
+        return this.salmentak*this.komisioa;
+    }
+
+
+}
