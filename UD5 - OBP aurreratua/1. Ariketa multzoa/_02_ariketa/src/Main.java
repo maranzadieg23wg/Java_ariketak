5d879187eb129a64925290a1fcde6c56ab21b095
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-01-11 11:13:06 +0100

    Proba datuak ongi jarrita
    Proba datuak gehituta eta funtzionatzen arazorik gabe.

--- UD5 - OBP aurreratua/1. Ariketa multzoa/_02_ariketa/src/Main.java
+++ UD5 - OBP aurreratua/1. Ariketa multzoa/_02_ariketa/src/Main.java
@@ -4,5 +4,28 @@ public class Main {
     public static void main(String[] args) {
 
         Liburu l1 = new Liburu(1, "Cuentos de terramar", "Ursula K.", 150);
+
+        Bezero b1 = new Bezero("12345678T", "Patxi");
+
+        Mailegu m1 = new Mailegu(2024,1,11, l1);
+
+        b1.gehituMailegu(m1);
+        b1.gehituMailegu(m1);
+        b1.gehituMailegu(m1);
+        b1.gehituMailegu(m1);
+        b1.gehituMailegu(m1);
+        //b1.gehituMailegu(m1);
+        b1.erakutsi();
+
+
+        b1.kenduMailegu(m1);
+
+        b1.erakutsi();
+
+        b1.gehituMailegu(m1);
+        b1.gehituMailegu(m1);
+        b1.erakutsi();
+
+
     }
 }
