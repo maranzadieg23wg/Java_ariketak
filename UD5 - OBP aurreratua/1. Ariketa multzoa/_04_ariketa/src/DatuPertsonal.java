8c4748726ec4c84cbfdbd0d68392e2c6bce5ca0b
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-01-17 09:13:00 +0100

    DatuPertsonal klasea sortuta
    Sortu egin da DatuPertsonal klasea eta honek behar dituen funtzioak eta eraikitzailea.

---
+++ UD5 - OBP aurreratua/1. Ariketa multzoa/_04_ariketa/src/DatuPertsonal.java
@@ -0,0 +1,26 @@
+public class DatuPertsonal {
+
+    private String txorilzena;
+    private String jabelzena;
+
+    DatuPertsonal(String txorilzena, String jabelzena) {
+        this.txorilzena = txorilzena;
+        this.jabelzena = jabelzena;
+    }
+
+    public String getTxorilzena() {
+        return txorilzena;
+    }
+
+    public void setTxorilzena(String txorilzena) {
+        this.txorilzena = txorilzena;
+    }
+
+    public String getJabelzena() {
+        return jabelzena;
+    }
+
+    public void setJabelzena(String jabelzena) {
+        this.jabelzena = jabelzena;
+    }
+}
