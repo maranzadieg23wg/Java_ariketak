6241d8ec4d906d5f408480d150c505978657aa3c
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-01-17 08:40:22 +0100

    Main probak
    Main klasean sortu egin dira objetuak egiteko probak ikusteko nola funtzionatzen duten.

--- UD5 - OBP aurreratua/1. Ariketa multzoa/_05_ariketa/src/Main.java
+++ UD5 - OBP aurreratua/1. Ariketa multzoa/_05_ariketa/src/Main.java
@@ -1,7 +1,14 @@
+import enpresa.Langile;
+import enpresa.Mugikor;
+
 public class Main {
     public static void main(String[] args) {
 
+        Mugikor m1 = new Mugikor("qwerty", 100);
+        Langile l1 = new Langile("Patxi", "12345678R", 175, 85, 0, m1);
 
+        l1.lanEgin();
+        System.out.println(l1.toString());
 
     }
 }
