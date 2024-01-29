3e3aff2cd6c4317bb63dd90749fdd616aaf22989
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-01-29 08:55:11 +0100

    Espezialitate enum sortuta
    Sortu egin da espezialitate enum klasea

--- UD6 - Datu-Egitura Dinamikoak/1.Ariketa multzoa/_05_ariketa/src/Kirolari.java
+++ UD6 - Datu-Egitura Dinamikoak/1.Ariketa multzoa/_05_ariketa/src/Kirolari.java
@@ -4,14 +4,11 @@ public class Kirolari {
 
     private String izena;
     private int adina;
-    private enum Espezialitatea{
-        KORRIKALARI, TXIRRINDULARI, IGERILARI;
 
-    }
 
-    private  Espezialitatea espezialitatea1;
+    private  Espezialitate espezialitatea1;
 
-    public Kirolari(String izena, int adina, Espezialitatea espezialitatea) {
+    public Kirolari(String izena, int adina, Espezialitate espezialitatea) {
         this.izena = izena;
         this.adina = adina;
         this.espezialitatea1 = espezialitatea;
---
+++ UD6 - Datu-Egitura Dinamikoak/1.Ariketa multzoa/_05_ariketa/src/Txapelketa.java
@@ -0,0 +1,21 @@
+import java.util.HashSet;
+
+public class Txapelketa {
+
+
+    private HashSet<Espezialitate> KORRIKALARI;
+    private HashSet<Espezialitate> TXIRRINDULARI;
+    private HashSet<Espezialitate> IGERILARI;
+
+    public Txapelketa() {
+        this.KORRIKALARI = new HashSet<Espezialitate>();
+        this.TXIRRINDULARI = new HashSet<Espezialitate>();
+        this.IGERILARI = new HashSet<Espezialitate>();
+    }
+
+
+    
+
+
+
+}
