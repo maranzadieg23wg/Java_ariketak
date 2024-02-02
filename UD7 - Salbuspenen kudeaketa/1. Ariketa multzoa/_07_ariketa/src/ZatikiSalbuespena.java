35c36eeede9a1c71ad5f46754c004f5dd8d908da
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-02-02 09:49:25 +0100

    Ez du funtzionatzen
    Konpondu behar da

--- UD7 - Salbuspenen kudeaketa/1. Ariketa multzoa/_07_ariketa/src/ZatikiSalbuespena.java
+++ UD7 - Salbuspenen kudeaketa/1. Ariketa multzoa/_07_ariketa/src/ZatikiSalbuespena.java
@@ -1,2 +1,10 @@
-public class ZatikiSalbuespena {
+public class ZatikiSalbuespena extends Exception{
+
+    Karaktereak(){
+        super("Ezin dira karaktereak jarri, zenbakiak izan behar dira.");
+    }
+
+
+
+
 }
