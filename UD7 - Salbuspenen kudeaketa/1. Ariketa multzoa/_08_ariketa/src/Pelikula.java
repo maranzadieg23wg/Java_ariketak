9c369bd2df15339c513afc599c9f5b30c3779113
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-02-05 09:38:53 +0100

    Konponduta
    Errore bat agertzean, kode guztia gelditu egiten da.

--- UD7 - Salbuspenen kudeaketa/1. Ariketa multzoa/_08_ariketa/src/Pelikula.java
+++ UD7 - Salbuspenen kudeaketa/1. Ariketa multzoa/_08_ariketa/src/Pelikula.java
@@ -7,61 +7,33 @@ public class Pelikula {
     private int iraupena; //← minututan
 
     public Pelikula(String izenburua, int urtea, int iraupena)throws PelikulaSalbuespena {
-        try {
 
-            if (izenburua == null || izenburua.equals("")){
-                throw new PelikulaSalbuespena("Pelikula batek ezin du ez izan izenik");
-            }
-
-        }catch (PelikulaSalbuespena e){
-            System.err.println("Errore bat gertatu da: "+e.getMessage());
+        if (izenburua == null || izenburua.equals("")){
+            throw new PelikulaSalbuespena("Pelikula batek ezin du ez izan izenik");
         }
 
-        try {
-
-            if (zenbakiaDa(String.valueOf(iraupena))){
-                iraupena=Integer.parseInt(String.valueOf(iraupena));
-            }else {
-                throw new PelikulaSalbuespena("Pelikula baten iraupena ezin da izan letra bat");
-            }
-
-        }catch (PelikulaSalbuespena a){
-            System.err.println("Errore bat gertatu da: "+a.getMessage());
-
+        if (zenbakiaDa(String.valueOf(iraupena))){
+            iraupena=Integer.parseInt(String.valueOf(iraupena));
+        }else {
+            throw new PelikulaSalbuespena("Pelikula baten iraupena ezin da izan letra bat");
         }
 
-        try {
+        if (zenbakiaDa(String.valueOf(urtea))){
+            urtea=Integer.parseInt(String.valueOf(urtea));
 
-            if (zenbakiaDa(String.valueOf(urtea))){
-                urtea=Integer.parseInt(String.valueOf(urtea));
-
-            }else {
-                throw new PelikulaSalbuespena("Pelikula batek urtea ezin da izan letra bat");
-            }
-        }catch (PelikulaSalbuespena a){
-            System.err.println("Errore bat gertatu da: "+a.getMessage());
-
+        }else {
+            throw new PelikulaSalbuespena("Pelikula batek urtea ezin da izan letra bat");
         }
 
-        try {
-            if (urtea<1900){
-                //System.out.println("111111111111111111"+izenburua);
-                throw new PelikulaSalbuespena("Pelikula baten urtea ezin da 1900 baino lehenagokoa izan");
-            }else {
-                //System.out.println("22222222222222222222222222"+izenburua);
-            }
-        }catch (PelikulaSalbuespena a){
-            System.err.println("Errore bat gertatu da: "+a.getMessage());
-
+        if (urtea<1900){
+            //System.out.println("111111111111111111"+izenburua);
+            throw new PelikulaSalbuespena("Pelikula baten urtea ezin da 1900 baino lehenagokoa izan");
+        }else {
+            //System.out.println("22222222222222222222222222"+izenburua);
         }
 
-        try {
-            if (iraupena<=0){
-                throw new PelikulaSalbuespena("Pelikula baten iraupena ezin da 0 baino txikiagoa izan");
-            }
-        }catch (PelikulaSalbuespena a){
-            System.err.println("Errore bat gertatu da: "+a.getMessage());
-
+        if (iraupena<=0){
+            throw new PelikulaSalbuespena("Pelikula baten iraupena ezin da 0 baino txikiagoa izan");
         }
 
 
