561f8d54defffb1aa11ea498ff4bda4bda7598df
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-02-08 19:31:42 +0100

    Sortu egin da HashMap-a eta menua
    Horain egin behar dira funtzio guztiak.

--- Azterketa/2.Ebaul/_F1_Manex_Aranzadi_egana_3.0/src/Main.java
+++ Azterketa/2.Ebaul/_F1_Manex_Aranzadi_egana_3.0/src/Main.java
@@ -1,5 +1,55 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        Scanner sc = new Scanner(System.in);
+
+        HashMap<String, HashMap<Integer, Langile>> F1 = new HashMap<>();
+
+        int z =0;
+
+        while (z !=6){
+
+
+            System.out.println("1: Erakutsi");
+            System.out.println("2: Gehitu talde bat");
+            System.out.println("3: Ezabatu talde bat");
+            System.out.println("4: Gehitu langile bat");
+            System.out.println("5: Aldatu langile bat talde batetik");
+            System.out.println("6: Bukatu programa");
+
+            z = sc.nextInt();
+
+            switch (z){
+                case 1:
+                    erakutsi(F1);
+                    break;
+                case 2:
+                    gehituTaldea(F1);
+                    break;
+                case 3:
+                    borratuTaldea(F1);
+                    break;
+                case 4:
+                    gehituLangile(F1);
+                    break;
+                case 5:
+
+                    F1=aldatuLangile(F1);
+                    break;
+                default:
+                    System.out.println("Sartutako zenbakiak ez du ezer egiten");
+                    break;
+
+            }
+
+        }
+
     }
+
+
+
 }
