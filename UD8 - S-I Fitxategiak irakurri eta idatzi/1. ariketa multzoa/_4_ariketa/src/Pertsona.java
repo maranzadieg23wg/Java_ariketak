0806ac52cfa32062d98fc90badc17397bbd75327
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-02-16 09:23:03 +0100

    Sortuta Pertsona klasea
    Sortu egin da Pertsona klasea eta honekin etortzen diren bariableak eta eraikitzailea.

---
+++ UD8 - S-I Fitxategiak irakurri eta idatzi/1. ariketa multzoa/_4_ariketa/src/Pertsona.java
@@ -0,0 +1,11 @@
+public class Pertsona {
+    private String izena;
+    private String telefonoa;
+    private String herria;
+
+    public Pertsona(String izena, String telefonoa, String herria) {
+        this.izena = izena;
+        this.telefonoa = telefonoa;
+        this.herria = herria;
+    }
+}
