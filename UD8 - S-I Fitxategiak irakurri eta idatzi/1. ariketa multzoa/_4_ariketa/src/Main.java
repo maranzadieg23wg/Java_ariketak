cca7040edfab3ba06161115d7e99cf26c8b97124
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-02-16 09:31:05 +0100

    Try Catch gehituta
    Gehitu egin da try Catch ez egoteko arazorik

--- UD8 - S-I Fitxategiak irakurri eta idatzi/1. ariketa multzoa/_4_ariketa/src/Main.java
+++ UD8 - S-I Fitxategiak irakurri eta idatzi/1. ariketa multzoa/_4_ariketa/src/Main.java
@@ -1,5 +1,29 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        try {
+            BufferedReader f1 = new BufferedReader(new FileReader(".\\fitxategiak\\pertsona.txt"));
+
+
+
+
+
+
+
+        }catch (IOException e){
+            System.err.println("Fitxategia ez da aurkitu");
+        }
+
+
+
+
+
+
+
+
     }
 }
