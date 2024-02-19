09923f95bd25686f8e9ba21dab16ede48ecb7a97
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-02-19 09:20:22 +0100

    Ariketa bukatuta gehituta try-catch-al
    Gehitu egin dira behar diren try-catch-ak eta funtzionatzen du arazorik gabe.

--- UD8 - S-I Fitxategiak irakurri eta idatzi/1. ariketa multzoa/_4_ariketa/src/AgendaKudeatzaile.java
+++ UD8 - S-I Fitxategiak irakurri eta idatzi/1. ariketa multzoa/_4_ariketa/src/AgendaKudeatzaile.java
@@ -9,57 +9,86 @@ public class AgendaKudeatzaile {
 
     //private  BufferedReader f1;
 
-    public AgendaKudeatzaile(String izena) {
+    public AgendaKudeatzaile(String izena) throws IOException {
         this.izena = izena;
+        BufferedReader f1 = null;
         try {
-            BufferedReader f1 = new BufferedReader(new FileReader(".\\agenda\\"+this.izena));
-        }catch (IOException e){
-            System.err.println("Fitxategia ez da aurkitu");
+            f1 = new BufferedReader(new FileReader(".\\agenda\\"+this.izena));
+        }catch (FileNotFoundException e){
+            throw new Salbuespenak("Ezin da "+izena+"-ko agenda aurkitu");
+        }finally {
+            f1.close();
         }
+
 
     }
 
 
     void erakutsi() throws IOException {
-
-        BufferedReader f1 = new BufferedReader(new FileReader(".\\agenda\\"+this.izena));
-        String line;
-        while ((line = f1.readLine()) !=null){
-            System.out.println(line);
+        BufferedReader f1 = null;
+        try {
+            f1 = new BufferedReader(new FileReader(".\\agenda\\"+this.izena));
+            String line;
+            while ((line = f1.readLine()) !=null){
+                System.out.println(line);
+            }
+            f1.close();
+        }catch (IOException e){
+            throw new Salbuespenak("Ezin izan da ireki fitxategia eta ikusi bertan dauden kontaktuak");
+        }finally {
+            f1.close();
         }
-        f1.close();
+
 
     }
 
     void gehituPertsona(Pertsona pertsona) throws IOException {
+        BufferedWriter w1 = null;
+        try {
+            w1 = new BufferedWriter(new FileWriter(".\\agenda\\"+this.izena, true));
 
-        BufferedWriter w1 = new BufferedWriter(new FileWriter(".\\agenda\\"+this.izena, true));
+            String izena = pertsona.getIzena();
+            long telefonoa = pertsona.getTelefonoa();
+            String herria = pertsona.getHerria();
 
-        String izena = pertsona.getIzena();
-        long telefonoa = pertsona.getTelefonoa();
-        String herria = pertsona.getHerria();
-
-        //System.out.println(izena+ telefonoa+ herria);
-        w1.newLine();
-        w1.write(izena+"    "+telefonoa+"   "+herria);
-        w1.close();
+            //System.out.println(izena+ telefonoa+ herria);
+            w1.newLine();
+            w1.write(izena+" "+telefonoa+" "+herria);
+            w1.close();
+        }catch (IOException e){
+            throw new Salbuespenak("Ezin izan da gehitu "+izena+" pertsonaren datuak "+izena+" agendara.");
+        }finally {
+            w1.close();
+        }
 
     }
 
     Pertsona pertsonaBilatu(String izena) throws IOException {
+        BufferedReader f1 = null;
+        try {
+            f1 = new BufferedReader(new FileReader(".\\agenda\\"+this.izena));
+            String line;
+            while ((line = f1.readLine()) !=null){
 
-        BufferedReader f1 = new BufferedReader(new FileReader(".\\agenda\\"+this.izena));
-        String line;
-        while ((line = f1.readLine()) !=null){
+                String[] datuak = line.split(" ");
 
-            String[] datuak = line.split(" ");
-
-            if (datuak[0].equals(izena)){
-                return new Pertsona(datuak[0], parseLong(datuak[1]), datuak[2]);
+                if (datuak[0].equals(izena)){
+                    f1.close();
+                    return new Pertsona(datuak[0], parseLong(datuak[1]), datuak[2]);
+                }
             }
+            f1.close();
+
+            return null;
+        }catch (IOException e){
+            throw new Salbuespenak("Ezin izan da ireki "+izena+" agenda");
+        }catch (NumberFormatException e){
+            throw new Salbuespenak("Bilatzen harin den pertsonak daturen bat gaizki dauka sartuta");
         }
-        f1.close();
-        return null;
+        finally {
+            f1.close();
+        }
+
 
 
 
--- UD8 - S-I Fitxategiak irakurri eta idatzi/1. ariketa multzoa/_4_ariketa/src/Main.java
+++ UD8 - S-I Fitxategiak irakurri eta idatzi/1. ariketa multzoa/_4_ariketa/src/Main.java
@@ -5,29 +5,29 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args){
 
-        AgendaKudeatzaile agenda = new AgendaKudeatzaile("agenda.txt");
 
-        //agenda.erakutsi();
-
-        //Pertsona pertsona1 = new Pertsona("Manolo", 1231231231, "Sevilla");
-        //agenda.gehituPertsona(pertsona1);
-        agenda.erakutsi();
-
-
-        Pertsona proba = agenda.pertsonaBilatu("Jacques");
-
-        if (proba != null){
-            System.out.println(proba.toString());
-        }else {
-            System.err.println("Bilatutako pertsona ez dago agendan");
-        }
 
         try {
 
+            AgendaKudeatzaile agenda = new AgendaKudeatzaile("agenda.txt");
 
+            //agenda.erakutsi();
+
+            //Pertsona pertsona1 = new Pertsona("Manolo", 1231231231, "Sevilla");
+            //agenda.gehituPertsona(pertsona1);
+            //agenda.erakutsi();
+
+
+            Pertsona proba = agenda.pertsonaBilatu("Manolo");
+
+            if (proba != null){
+                System.out.println(proba.toString());
+            }else {
+                System.err.println("Bilatutako pertsona ez dago agendan");
+            }
 
         }catch (IOException e){
-            System.err.println("Arazoren bat gertatu da.");
+            System.err.println("Errore bat gertatu da: "+e.getMessage());
         }
 
 
