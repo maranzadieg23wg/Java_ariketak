1fed1ff5191609923b84100125d473d796c6b044
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-02-19 09:38:52 +0100

    Mugikor klasea sortuta
    Sortu egin da mugikor klasea eta honekin batera behar dituen funtzioa guztiak.

---
+++ UD8 - S-I Fitxategiak irakurri eta idatzi/2. ariketa multzoa/_1_ariketa/src/Mugikor.java
@@ -0,0 +1,40 @@
+public class Mugikor {
+
+    private long telefonoa;
+    private int bateria;
+
+    public Mugikor(long telefonoa, int bateria) {
+        this.telefonoa = telefonoa;
+        this.bateria = bateria;
+    }
+
+    void erakutsi(){
+        System.out.println(toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Mugikor{" +
+                "telefonoa=" + telefonoa +
+                ", bateria=" + bateria +
+                '}';
+    }
+
+    void kargatu(int bateria){
+        setBateria(bateria);
+    }
+
+    void deitu(int minutu){
+        int bateria = getBateria();
+
+        setBateria(minutu*2-bateria);
+    }
+
+    private void setBateria(int bateria) {
+        this.bateria = bateria;
+    }
+
+    public int getBateria() {
+        return bateria;
+    }
+}
