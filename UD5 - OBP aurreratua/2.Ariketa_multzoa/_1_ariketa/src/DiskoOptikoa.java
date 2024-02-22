32a4752d25f05ac77a6ba1309a8e6494e8c40e81
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-02-22 11:38:47 +0100

    Aldatu egin da nola funtzionatzen duen kode guztia
    Horain 3 herentzia daude.

--- UD5 - OBP aurreratua/2.Ariketa_multzoa/_1_ariketa/src/DiskoOptikoa.java
+++ UD5 - OBP aurreratua/2.Ariketa_multzoa/_1_ariketa/src/DiskoOptikoa.java
@@ -1,18 +1,14 @@
-public abstract class DiskoOptikoa {
+public abstract class DiskoOptikoa extends Grabatzeko {
 
-    public double edukiera;
-    public String unitatea;
-    public int okupatutakoPortz;
+
     public boolean birgrabagarria;
 
     public DiskoOptikoa(double edukiera, String unitatea, int okupatutakoPortz, boolean birgrabagarria) {
-        this.edukiera = edukiera;
-        this.unitatea = unitatea;
-        this.okupatutakoPortz = okupatutakoPortz;
+        super(edukiera, unitatea,okupatutakoPortz);
         this.birgrabagarria = birgrabagarria;
     }
 
-    public abstract void irakurri();
-    public abstract void grabatu(double kantitatea);
+
+
 
 }
--- UD5 - OBP aurreratua/2.Ariketa_multzoa/_1_ariketa/src/Main.java
+++ UD5 - OBP aurreratua/2.Ariketa_multzoa/_1_ariketa/src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+
+        CD cd = new CD(100);
+        DVD dvd = new DVD(200);
+        SSD ssd = new SSD(1000);
+        Binilo binilo = new Binilo(50);
+        Frisbee frisbee = new Frisbee();
+
+        cd.biratu();
+        dvd.biratu();
+        binilo.biratu();
+        frisbee.biratu();
+
+        cd.grabatu(100);
+        ssd.grabatu(1001);
+
+
+
+
+
+
+
+
+
+
     }
 }
