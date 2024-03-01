90347d80c90fa06995453272111a41799a6f5ac7
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-03-01 09:04:59 +0100

    Ariketa eginda
    Funtzionatzen du arazorik gabe

--- UD8 - S-I Fitxategiak irakurri eta idatzi/2. ariketa multzoa/_2_ariketa/src/Main.java
+++ UD8 - S-I Fitxategiak irakurri eta idatzi/2. ariketa multzoa/_2_ariketa/src/Main.java
@@ -1,5 +1,20 @@
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+
+        DataInputStream dis = new DataInputStream(new FileInputStream("./img/guts.jpg"));
+        DataOutputStream dos = new DataOutputStream((new FileOutputStream("./img/guts2.jpg")));
+
+
+        int zenbat = dis.available();
+
+        for (int i =0;i<zenbat;i++){
+            //System.out.println(dis.readByte());
+            dos.writeByte(dis.readByte());
+        }
+        System.out.println("Gordeta");
+
     }
 }
