2676800f8908c257fca1707ad2cc6807ada87f70
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-03-17 12:03:40 +0100

    Ariketa egiten

---
+++ .Mugarriak/_1_Mugarria/src/Lehioa.java
@@ -0,0 +1,97 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Lehioa {
+
+private int x;
+private int y;
+private String izena;
+
+private JFrame frame;
+private JButton botoia1, botoia2;
+private JRadioButton radio1, radio2, radio3;
+private JCheckBox chex1, chex2;
+private JPanel behekoPanel, goikoPanel, eskubikoPanel, ezkerrekoPanel;
+
+
+    public Lehioa(String izena, int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.izena = izena;
+
+        sortuLehoia();
+
+        beheko();
+        eskubiko();
+        goiko();
+
+
+        erakutsi();
+
+
+
+
+
+
+    }
+
+
+    void sortuLehoia(){
+        frame = new JFrame(izena);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(x, y));
+    }
+    void erakutsi(){
+        frame.add(behekoPanel, BorderLayout.SOUTH);
+        frame.add(behekoPanel, BorderLayout.WEST);
+
+        frame.add(eskubikoPanel, BorderLayout.EAST);
+        frame.add(goikoPanel, BorderLayout.NORTH);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    void beheko(){
+        behekoPanel = new JPanel();
+        botoia1 = new JButton("Botoia 1");
+        botoia2 = new JButton("Botoia 2");
+        behekoPanel.setLayout(new GridLayout(1, 10));
+        behekoPanel.setLayout(new FlowLayout());
+        behekoPanel.setBackground(Color.blue);
+
+        behekoPanel.add(botoia1);
+        behekoPanel.add(botoia2);
+
+    }
+
+    void eskubiko(){
+        eskubikoPanel = new JPanel();
+        radio1 = new JRadioButton("OPT 1", true);
+        radio2 = new JRadioButton("OPT 2", false);
+        radio3 = new JRadioButton("OPT 3", false);
+        eskubikoPanel.setLayout(new BoxLayout(eskubikoPanel, BoxLayout.Y_AXIS));
+        //eskubikoPanel.setBackground(Color.blue);
+
+        ButtonGroup radio = new ButtonGroup();
+        radio.add(radio1);
+        radio.add(radio2);
+        radio.add(radio3);
+        eskubikoPanel.add(radio1);
+        eskubikoPanel.add(radio2);
+        eskubikoPanel.add(radio3);
+
+
+    }
+
+    void goiko(){
+        goikoPanel = new JPanel();
+
+        chex1 = new JCheckBox("Katniss");
+        chex2 = new JCheckBox("Peeta");
+
+        goikoPanel.add(chex1);
+        goikoPanel.add(chex2);
+
+    }
+}
---
+++ .Mugarriak/_1_Mugarria/src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        Lehioa m1 = new Lehioa("Manex", 600, 300);
+
+
+    }
+}
