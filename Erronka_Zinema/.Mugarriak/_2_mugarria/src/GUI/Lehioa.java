78b83e02504577b1a8f4ce4ebaa81ee780a5e807
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-03-21 17:53:48 +0100

    Gehituta textArea eta listenerrak
    Gehitu egin dira textArea eta listenerrak

--- Erronka_Zinema/.Mugarriak/_2_mugarria/src/GUI/Lehioa.java
+++ Erronka_Zinema/.Mugarriak/_2_mugarria/src/GUI/Lehioa.java
@@ -1,8 +1,10 @@
 package GUI;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Lehioa {
+public class Lehioa extends JFrame implements ActionListener {
 
     private int x;
     private int y;
@@ -12,9 +14,10 @@ public class Lehioa {
     private JFrame frame;
     private JPanel menua;
     private JComboBox<String> lista;
-    private JButton garbitu;
-    private JTextField textua;
-    
+    private JButton garbitu, itzi;
+    private JTextArea textua;
+    private JScrollPane scrola;
+
         public Lehioa(String izena, int x, int y){
 
             this.x = x;
@@ -49,8 +52,9 @@ public class Lehioa {
         void menua(){
 
             menua = new JPanel();
+            //menua.setLayout(new BorderLayout());
 
-            //GridLayout gridLayout = new GridLayout(2,1);
+            //GridLayout gridLayout = new GridLayout(1,2);
             //menua.setLayout(gridLayout);
 
             menua.setBackground(Color.CYAN);
@@ -58,21 +62,53 @@ public class Lehioa {
 
             lista = new JComboBox<String>();
             lista.setBounds(100, 10, 150, 20);
-            lista.addItem("Gorria");
-            lista.addItem("Berdea");
-            lista.addItem("Horia");
+            lista.addItem("Python.txt");
+            lista.addItem("C.txt");
+            lista.addItem("Java.txt");
+            lista.addActionListener(this);
+
 
             lista.setPreferredSize(new Dimension(200, 25));
             menua.add(lista);
 
             garbitu = new JButton("Clear");
+            garbitu.addActionListener(this);
             menua.add(garbitu);
 
-            textua = new JTextField();
-            textua.setPreferredSize(new Dimension(300, 300));
-            menua.add(textua);
+
+
+
+
+
+            textua = new JTextArea();
+            textua.setPreferredSize(new Dimension(400, 600));
+
+            textua.setLineWrap(true);
+
+            scrola = new JScrollPane(textua); //← Scrola sortu
+            scrola.setBounds(10,50,400,300);
+
+
+            //textua.setVerticalAlignment(JTextField.TOP);
+            menua.add(scrola);
+
+            itzi = new JButton("Close");
+            itzi.addActionListener(this);
+            menua.add(itzi);
+
 
 
         }
-    
+
+        public void actionPerformed(ActionEvent e){
+            if (e.getSource() == garbitu) {
+                textua.setText("");
+            } else if (e.getSource() == itzi) {
+                System.exit(0);
+            }else if (e.getSource() == lista) {
+                String selectedOption = (String) lista.getSelectedItem();
+                textua.append(selectedOption + "\n");
+            }
+
+        }
 }
