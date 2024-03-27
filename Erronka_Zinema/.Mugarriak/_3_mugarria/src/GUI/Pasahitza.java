383f7d75f723791c3e464266c7323823f90eae63
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-03-27 12:00:54 +0100

    Sortuta login-a, funtzionatzen du
    Eskatzen da pasahitza bat, eta ongi sartzen badu, joan alko da urrengo leihora.

--- Erronka_Zinema/.Mugarriak/_3_mugarria/src/GUI/Pasahitza.java
+++ Erronka_Zinema/.Mugarriak/_3_mugarria/src/GUI/Pasahitza.java
@@ -1,13 +1,19 @@
 package GUI;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class Pasahitza {
 
 
     public Pasahitza() {
         String name = JOptionPane.showInputDialog("Input password");
+        if (name.equals("damocles")){
+            System.out.println("logeatuta");
+            
+        }
+
     }
 
-    
+
 }
