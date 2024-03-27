fae385c15a9062c6d455b65ed1204239ff12bb70
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-03-27 11:55:08 +0100

    Pasahitza pop up sortuta
    Sortu egin da agertzea pop UP-a pasahitza sartu al izateko

--- Erronka_Zinema/.Mugarriak/_3_mugarria/src/GUI/Pasahitza.java
+++ Erronka_Zinema/.Mugarriak/_3_mugarria/src/GUI/Pasahitza.java
@@ -1,6 +1,13 @@
 package GUI;
 
-public class Pasahitza {
-    
+import javax.swing.*;
 
+public class Pasahitza {
+
+
+    public Pasahitza() {
+        String name = JOptionPane.showInputDialog("Input password");
+    }
+
+    
 }
--- Erronka_Zinema/.Mugarriak/_3_mugarria/src/Main.java
+++ Erronka_Zinema/.Mugarriak/_3_mugarria/src/Main.java
@@ -1,5 +1,13 @@
+import GUI.Pasahitza;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        new Pasahitza();
+
+
+
+
     }
 }
