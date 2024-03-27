c3330e149d0d6eb1022fdf49106e9a2bffbe274e
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-03-27 12:55:28 +0100

    Gehituta irudia karpeta irakurtzea
    Gehitu egin da funtzioa gehitzeko karpetan dauden fitxategien izenak irakurtzea

--- Erronka_Zinema/.Mugarriak/_3_mugarria/src/GUI/Leihoa.java
+++ Erronka_Zinema/.Mugarriak/_3_mugarria/src/GUI/Leihoa.java
@@ -2,14 +2,22 @@ package GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
-public class Leihoa extends JFrame{
+public class Leihoa extends JFrame implements ActionListener {
 
     private int x;
     private int y;
     private String izena;
 
     private JFrame frame;
+    private JPanel menua, menua2;
+    private String[] lista;
+    private JComboBox<String> listaIrudiak;
+    private JTextArea textua;
+
 
     public Leihoa(String izena, int x, int y){
 
@@ -18,7 +26,7 @@ public class Leihoa extends JFrame{
         this.izena = izena;
 
         sortuLehioa();
-
+        irudiakList();
 
 
         erakutsi();
@@ -34,8 +42,53 @@ public class Leihoa extends JFrame{
 
     void erakutsi(){
 
+        frame.add(menua, BorderLayout.WEST);
+        frame.add(menua2, BorderLayout.EAST);
+
+
         frame.pack();
         frame.setVisible(true);
     }
 
+    void irudiakList(){
+        File karpeta = new File("IMG");
+
+        if (karpeta.exists() && karpeta.isDirectory()){
+            lista = karpeta.list();
+        }else {
+            System.err.println("Sartutako karpeta ez da existitzen");
+        }
+    }
+
+    void menua(){
+        menua = new JPanel();
+
+
+        listaIrudiak = new JComboBox<>();
+        for (String izenak : lista){
+            listaIrudiak.addItem(izenak);
+        }
+        listaIrudiak.addActionListener(this);
+        menua.add(listaIrudiak);
+
+        ImageIcon originalIcon = new ImageIcon("img\\"+listaIrudiak);
+        Image img = originalIcon.getImage();
+        Image img1 = img.getScaledInstance(125, 100, Image.SCALE_SMOOTH);
+        ImageIcon theDark = new ImageIcon(img1);
+        JLabel label = new JLabel();
+        label.setIcon(theDark);
+        menua.add(label);
+
+
+    }
+
+    void menua2(){
+        menua = new JPanel();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
