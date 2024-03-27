98598cd1488921f23fa299992d06eb5845093c7d
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-03-27 12:24:18 +0100

    Lehioa sortuta

--- Erronka_Zinema/.Mugarriak/_3_mugarria/src/GUI/Leihoa.java
+++ Erronka_Zinema/.Mugarriak/_3_mugarria/src/GUI/Leihoa.java
@@ -1,4 +1,41 @@
 package GUI;
 
-public class Leihoa {
+import javax.swing.*;
+import java.awt.*;
+
+public class Leihoa extends JFrame{
+
+    private int x;
+    private int y;
+    private String izena;
+
+    private JFrame frame;
+
+    public Leihoa(String izena, int x, int y){
+
+        this.x = x;
+        this.y = y;
+        this.izena = izena;
+
+        sortuLehioa();
+
+
+
+        erakutsi();
+
+    }
+
+    void sortuLehioa(){
+        frame = new JFrame(izena);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setPreferredSize(new Dimension(x, y));
+    }
+
+    void erakutsi(){
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
 }
