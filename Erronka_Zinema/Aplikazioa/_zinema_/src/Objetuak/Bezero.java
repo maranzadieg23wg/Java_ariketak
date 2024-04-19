6b04a63fb2a994a14182da526a64153bba3606df
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-04-19 09:33:05 +0200

    Bezeroa klasea sortuta
    Sortu egin da bezero klase bat

---
+++ Erronka_Zinema/Aplikazioa/_zinema_/src/Objetuak/Bezero.java
@@ -0,0 +1,22 @@
+package Objetuak;
+
+public class Bezero {
+
+    int idErabiltzailea;
+    String izena;
+    String abizena;
+    String emaila;
+    String erabiltzaileIzena;
+    String pasahitza;
+    int ikusitakoListaID;
+
+    public Bezero(int idErabiltzailea, String izena, String abizena, String emaila, String erabiltzaileIzena, String pasahitza, int ikusitakoListaID) {
+        this.idErabiltzailea = idErabiltzailea;
+        this.izena = izena;
+        this.abizena = abizena;
+        this.emaila = emaila;
+        this.erabiltzaileIzena = erabiltzaileIzena;
+        this.pasahitza = pasahitza;
+        this.ikusitakoListaID = ikusitakoListaID;
+    }
+}
