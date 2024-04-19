bdeb5dff3216e0f0490df5cd0925dcc182a54422
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-04-19 13:59:20 +0200

    Loggina egiten
    Bukatu behar da gehitu bezero berri bat

--- Erronka_Zinema/Aplikazioa/_zinema_/src/DatuBase/Conexioa.java
+++ Erronka_Zinema/Aplikazioa/_zinema_/src/DatuBase/Conexioa.java
@@ -75,14 +75,9 @@ public class Conexioa {
 
 
         //Begiratu ea existitzen den
-        String sql = "select count(*) from ERABILTZAILEAK where (emaila = ?)";
-        PreparedStatement kontsulta = conn.prepareStatement(sql);
-        kontsulta.setString(1, emaila);
+        //ezistitzenDa(emaila);
 
-
-        ResultSet emaitza = kontsulta.executeQuery();
-
-        if (emaitza.getInt(1)!=0){
+        if (ezistitzenDaEmail(emaila)){
             System.err.println(emaila+" erabiltzailearekin kontu bat existitzen da");
         }else {
 
@@ -94,23 +89,16 @@ public class Conexioa {
             String erabiltzailea = sc.nextLine();
 
 
-            ResultSet emaitza1 = null;
-            while (emaitza1.getInt(1) !=0){
-                String sql1 = "select count(*) from ERABILTZAILEAK where (erabiltzailea = ?)";
-                PreparedStatement kontsulta1 = conn.prepareStatement(sql1);
-                kontsulta1.setString(1, erabiltzailea);
 
+            while (ezistitzenDaErabiltzailea(erabiltzailea)){
 
-                 emaitza1 = kontsulta1.executeQuery();
 
                 System.out.println(erabiltzailea+" ezistitzenda.");
 
                 System.out.print("Erabiltzailea: ");
                 erabiltzailea = sc.nextLine();
 
-                if (emaitza.getInt(1) !=0){
-                    break;
-                }
+
             }
 
 
@@ -119,6 +107,7 @@ public class Conexioa {
 
             String sql2 = "Insert into ERABILTZAILEAK values (?, ?, ?, ?, ?, ?, ?)";
             PreparedStatement kontsulta2 = conn.prepareStatement(sql2);
+            kontsulta2.setString(1, azkenekoID());
             kontsulta2.setString(2, izena);
             kontsulta2.setString(3, abizena);
             kontsulta2.setString(4, emaila);
@@ -143,36 +132,74 @@ public class Conexioa {
     }
 
     void saioaHasi() throws SQLException {
-
+        sc.nextLine();
         System.out.println("-----------SAIOA HASI-----------");
         System.out.print("Emaila: ");
         String emaila = sc.nextLine();
         System.out.print("Pasahitza: ");
         String pasahitza = sc.nextLine();
-        int pasahitzaHash = pasahitza.hashCode();
-        pasahitza = null;
+        String pasahitzaHash = String.valueOf(pasahitza.hashCode());
 
-
-
-        String sql = "select count(*), * from ERABILTZAILEAK where (emaila = ? and pasahitza =?)";
+        String sql = "select * from ERABILTZAILEAK where emaila = ? and pasahitza = ?";
         PreparedStatement kontsulta = conn.prepareStatement(sql);
         kontsulta.setString(1, emaila);
-        kontsulta.setInt(2, pasahitzaHash);
+        kontsulta.setString(2, pasahitzaHash);
 
         ResultSet emaitza = kontsulta.executeQuery();
 
-        if (emaitza.getInt(1)!=0){
-            //System.err.println(emaila+" erabiltzailearekin kontu bat existitzen da");
-            int idErabiltzailea = emaitza.getInt(1);
-            String izena = emaitza.getString(2);
-            String abizena = emaitza.getString(3);
-            String email = emaitza.getString(4);
-            String erabiltzaile = emaitza.getString(5);
-            Bezero bezero = new Bezero(idErabiltzailea, izena, abizena, email, erabiltzaile);
+        if (emaitza.next()) {
             System.out.println("Saioa hasita");
-        }else {
-            System.err.println(emaila+" erabiltzailearekin kontu bat ez existitzen da");
+            int idErabiltzailea = emaitza.getInt("ID_ERABILTZAILEA");
+            String izena = emaitza.getString("IZENA");
+            String abizena = emaitza.getString("ABIZENA");
+            String email = emaitza.getString("EMAILA");
+            String erabiltzaile = emaitza.getString("ERABILTZAILEA");
+            Bezero bezero = new Bezero(idErabiltzailea, izena, abizena, email, erabiltzaile);
+        } else {
+            System.err.println(emaila + " erabiltzailearekin kontu bat ez dago existitzen");
         }
+    }
+
+
+    boolean ezistitzenDaEmail(String email) throws SQLException {
+        String sql = "select * from ERABILTZAILEAK where (emaila = ?)";
+        PreparedStatement kontsulta = conn.prepareStatement(sql);
+        kontsulta.setString(1, email);
+
+
+        ResultSet emaitza = kontsulta.executeQuery();
+
+        if (emaitza.next()){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    boolean ezistitzenDaErabiltzailea(String Erabiltzailea) throws SQLException {
+        String sql = "select * from ERABILTZAILEAK where (erabiltzailea = ?)";
+        PreparedStatement kontsulta = conn.prepareStatement(sql);
+        kontsulta.setString(1, Erabiltzailea);
+
+
+        ResultSet emaitza = kontsulta.executeQuery();
+
+        if (emaitza.next()){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    int azkenekoID() throws SQLException {
+        String sql = "select * from ERABILTZAILEAK WHERE ID_ERABILTZAILEA =(SELECT MAX(ID_ERABILTZAILEA) FROM ERABILTZAILEAK)";
+        PreparedStatement kontsulta = conn.prepareStatement(sql);
+
+
+
+        ResultSet emaitza = kontsulta.executeQuery();
+
+        return emaitza.getInt(1);
     }
 
 
--- Erronka_Zinema/Aplikazioa/_zinema_/src/Main.java
+++ Erronka_Zinema/Aplikazioa/_zinema_/src/Main.java
@@ -7,9 +7,9 @@ public class Main {
     public static void main(String[] args) throws SQLException {
 
 
-        //Index i1 = new Index(1500, 800, "zinema");
+        Index i1 = new Index(1500, 800, "zinema");
 
-        Conexioa c1 = new Conexioa();
+        //Conexioa c1 = new Conexioa();
 
     }
 }
