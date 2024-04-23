b5862df36b11051f5ff427cba5950a824f65047b
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-04-23 13:50:56 +0200

    Aktoreak objetua sortu egin da
    Aktoreak objetua sortu egin da

---
+++ Erronka_Zinema/Aplikazioa/_zinema_/src/Objetuak/Aktoreak.java
@@ -0,0 +1,39 @@
+package Objetuak;
+
+public class Aktoreak {
+
+    private int id_aktora;
+    private String izena;
+    private String abizena;
+    private String jaiotze_Data;
+    private String nazionalitatea;
+    private String emaila;
+    private String telefonoa;
+    private String irudia;
+    String irudiaLokalki;
+
+    public Aktoreak(int id_aktora, String izena, String abizena, String jaiotze_Data, String nazionalitatea, String emaila, String telefonoa, String irudia) {
+        this.id_aktora = id_aktora;
+        this.izena = izena;
+        this.abizena = abizena;
+        this.jaiotze_Data = jaiotze_Data;
+        this.nazionalitatea = nazionalitatea;
+        this.emaila = emaila;
+        this.telefonoa = telefonoa;
+        this.irudia = irudia;
+    }
+
+    public void setirudiaLokalki(String irudiaLokalki) {
+        this.irudiaLokalki = irudiaLokalki;
+    }
+
+    public String getIrudiaLokalki() {
+        return irudiaLokalki;
+    }
+
+    public String getIrudia() {
+        return irudia;
+    }
+
+
+}
