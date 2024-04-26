61885bda28917856a689e3f4e7505b2f56dc57f1
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-04-26 08:07:10 +0200

    Test objetua sortuta
    Sortu egin da test objetua testekin.
    Testetan logika gehitu behar da

---
+++ Erronka_Zinema/Aplikazioa/_zinema_/test/DatuBase/ConexioaTest.java
@@ -0,0 +1,118 @@
+package DatuBase;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConexioaTest {
+
+
+
+    @Test
+    void sortuKontua() {
+    }
+
+
+
+    @Test
+    void saioaHasi() {
+    }
+
+
+
+    @Test
+    void ezistitzenDaEmail() {
+    }
+
+    @Test
+    void ezistitzenDaErabiltzailea() {
+    }
+
+    @Test
+    void pelikulaLortu() {
+    }
+
+    @Test
+    void testPelikulaLortu() {
+    }
+
+    @Test
+    void testPelikulaLortu1() {
+    }
+
+    @Test
+    void pelikulaObjetua() {
+    }
+
+    @Test
+    void ikusitakoPelikulak() {
+    }
+
+    @Test
+    void ikusitakoListaLortu() {
+    }
+
+    @Test
+    void gehituIkusitakoPeli() {
+    }
+
+    @Test
+    void listanDago() {
+    }
+
+    @Test
+    void notaAldatu() {
+    }
+
+    @Test
+    void listatikanBorratu() {
+    }
+
+    @Test
+    void aktoreaLortu() {
+    }
+
+    @Test
+    void testAktoreaLortu() {
+    }
+
+    @Test
+    void testAktoreaLortu1() {
+    }
+
+    @Test
+    void aktoreakObjetua() {
+    }
+
+    @Test
+    void azkenekoID() {
+    }
+
+    @Test
+    void azkenekoPelikula() {
+    }
+
+    @Test
+    void lehenengoPelikula() {
+    }
+
+    @Test
+    void azkenekoAktorea() {
+    }
+
+    @Test
+    void lehenengoAktorea() {
+    }
+
+    @Test
+    void konexiaEgin() {
+    }
+
+    @Test
+    void konexioaItxi() {
+    }
+
+    @Test
+    void getBezero() {
+    }
+}
