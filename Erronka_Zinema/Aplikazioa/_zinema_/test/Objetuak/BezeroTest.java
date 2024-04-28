94f8881bee246480907f43e4eb5beaae8fc0c6df
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-04-28 12:20:59 +0200

    Bezero klasearen testa sortuta
    Sortu egin da testa, konprobatzeko ea ongi funtzionatzen duen klaseak

---
+++ Erronka_Zinema/Aplikazioa/_zinema_/test/Objetuak/BezeroTest.java
@@ -0,0 +1,23 @@
+package Objetuak;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BezeroTest {
+
+    Bezero bezero =new Bezero(1,"NIGHT", "WISH", "NIGHTWISH@gmail.com", "NIGHTWISH ");
+
+    @Test
+    @DisplayName("Erabiltzailearen ID-a egokia den konprobatu egiten da.")
+    void getIdErabiltzailea() {
+        assertEquals(1, bezero.getIdErabiltzailea(), "[-]Lortu egin den ID-ak ez da berbera");
+    }
+
+    @Test
+    @DisplayName("Erabiltzailearen izena egokia den konprobatu egiten da.")
+    void getErabiltzaileIzena() {
+        assertEquals("NIGHT", bezero.getErabiltzaileIzena(), "[-]Lortu egin den izena ez da berbera");
+    }
+}
