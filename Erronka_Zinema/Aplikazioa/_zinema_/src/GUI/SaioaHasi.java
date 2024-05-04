81630144c2f94396a1b99a1c1f0f8bc8c3b99e05
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-05-04 11:42:17 +0200

    Gaizki sartzen badira datuak, berriro saiatzen utzi beharko zuen
    Teorian

--- Erronka_Zinema/Aplikazioa/_zinema_/src/GUI/SaioaHasi.java
+++ Erronka_Zinema/Aplikazioa/_zinema_/src/GUI/SaioaHasi.java
@@ -10,34 +10,37 @@ public class SaioaHasi {
 
 
     public static Bezero saioaHasi() throws SQLException {
-        JTextField emaila = new JTextField();
-        JTextField pasahitza = new JPasswordField();
-        Object[] message = {
-                "Email:", emaila,
-                "Pasahitza:", pasahitza
-        };
+        
+        while (true){
+            JTextField emaila = new JTextField();
+            JTextField pasahitza = new JPasswordField();
+            Object[] message = {
+                    "Email:", emaila,
+                    "Pasahitza:", pasahitza
+            };
 
-        int option = JOptionPane.showConfirmDialog(null, message, "Saioa hasi", JOptionPane.OK_CANCEL_OPTION);
-        if (option == JOptionPane.OK_OPTION) {
+            int option = JOptionPane.showConfirmDialog(null, message, "Saioa hasi", JOptionPane.OK_CANCEL_OPTION);
+            if (option == JOptionPane.OK_OPTION) {
 
-            Konexioa conn = new Konexioa();
-            String email = emaila.getText();
-            String pas = pasahitza.getText();
-            if (conn.saioaHasi(email, pas)) {
-                System.out.println("Login successful");
+                Konexioa conn = new Konexioa();
+                String email = emaila.getText();
+                String pas = pasahitza.getText();
+                if (conn.saioaHasi(email, pas)) {
+                    System.out.println("Login successful");
 
-                return conn.getBezero();
+                    return conn.getBezero();
+                } else {
+                    JOptionPane.showMessageDialog(null, "Erabiltzailea edo pasaitza ez dira egokiak.", "Datuak gaizki sartuta", JOptionPane.ERROR_MESSAGE);
+
+                    System.out.println("login failed");
+
+                }
             } else {
-                System.out.println("login failed");
-
+                System.out.println("Login canceled");
                 return null;
             }
-        } else {
-            System.out.println("Login canceled");
-            return null;
         }
+
     }
-
-
 
 }
