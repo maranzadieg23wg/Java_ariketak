66a8c18b508dc08d5af181ba278853612f91fdb4
Manex <maranzadieg23wg@ikzubirimanteo.com>
2024-05-20 11:15:19 +0200

    Bertsio proba bat, hobetzen goango da

--- Erronka_Zinema/Aplikazioa/_zinema_/src/GUI/Informazioa/PelikulaInformazioa.java
+++ Erronka_Zinema/Aplikazioa/_zinema_/src/GUI/Informazioa/PelikulaInformazioa.java
@@ -15,84 +15,100 @@ import java.sql.SQLException;
 
 public class PelikulaInformazioa {
 
-
-
     public static JPanel peliInfo(Pelikulak pelikula) throws SQLException {
+        JPanel pelikularenInformazia = new JPanel(new BorderLayout());
 
-        JPanel pelikularenInformazia = new JPanel();
-
+        // Título de la película
         String izenburua = pelikula.getTituloa();
-        JLabel izenBuru = new JLabel(izenburua);
+        JLabel izenBuru = new JLabel(izenburua, SwingConstants.CENTER);
+        izenBuru.setFont(new Font("Arial", Font.BOLD, 20));
+        pelikularenInformazia.add(izenBuru, BorderLayout.NORTH);
 
-        pelikularenInformazia.add(izenBuru);
-
-
+        // Panel de datos
         JPanel datuak = pelikulaDatu(pelikula);
-
-        pelikularenInformazia.add(datuak);
-
-
+        pelikularenInformazia.add(datuak, BorderLayout.CENTER);
 
         return pelikularenInformazia;
-
     }
 
     static JPanel pelikulaDatu(Pelikulak pelikulak) throws SQLException {
+        JPanel datuak = new JPanel(new BorderLayout());
 
-        JPanel datuak = new JPanel();
+        // Panel izquierdo: Imagen
+        JLabel irudia = irudiak(pelikulak.getIrudiaLokalki(), 130, 200);
+        datuak.add(irudia, BorderLayout.WEST);
 
-        datuak.setLayout(new GridLayout(3, 2)); //↓→
+        // Panel derecho: Información y Sinopsis
+        JPanel infoYSinopsis = new JPanel(new BorderLayout());
 
-        datuak.add(irudiak(pelikulak.getIrudiaLokalki(), 130, 200));
-
+        // Panel de información
         JPanel informazioa = informazioa(pelikulak);
+        infoYSinopsis.add(informazioa, BorderLayout.NORTH);
 
-        datuak.add(informazioa);
-
-
+        // Sinopsis
         String sinopsis = pelikulak.getSinopsis();
-        JLabel sino = new JLabel(sinopsis);
-        datuak.add(sino);
+        JLabel sino = new JLabel("<html><body style='width: 300px'>" + sinopsis + "</body></html>");
+        infoYSinopsis.add(sino, BorderLayout.CENTER);
 
+        datuak.add(infoYSinopsis, BorderLayout.CENTER);
 
-        return  datuak;
+        // Panel inferior: Director y Nota
+        JPanel zuzendariEtaNota = new JPanel(new GridLayout(1, 2));
 
+        // Director
+        //String zuzendaria = pelikulak.getZuzendaria();
+        //JLabel zuzendariLabel = new JLabel("Zuzendaria: " + zuzendaria, SwingConstants.CENTER);
+        //zuzendariEtaNota.add(zuzendariLabel);
 
+        // Nota y añadir a lista
+        //JPanel notaGehitu = new JPanel(new BorderLayout());
+        //JLabel nota = new JLabel("Nota: " + pelikulak.getNota(), SwingConstants.CENTER);
+        JButton gehituLista = new JButton("Gehitu Lista");
+        //notaGehitu.add(nota, BorderLayout.CENTER);
+        //notaGehitu.add(gehituLista, BorderLayout.SOUTH);
+
+        //zuzendariEtaNota.add(notaGehitu);
+
+        //datuak.add(zuzendariEtaNota, BorderLayout.SOUTH);
+
+        // Panel inferior con actores
+       /* JPanel aktoreakPanel = new JPanel(new GridLayout(1, 7));
+        String[] aktoreak = pelikulak.getAktoreak().split(";"); // Asumiendo que los actores están separados por ";"
+        for (String aktore : aktoreak) {
+            aktoreakPanel.add(new JLabel(aktore, SwingConstants.CENTER));
+        }
+
+        datuak.add(aktoreakPanel, BorderLayout.SOUTH);*/
+
+        return datuak;
     }
 
-    static JPanel informazioa (Pelikulak pelikulak) throws SQLException {
-        JPanel informazioa = new JPanel();
+    static JPanel informazioa(Pelikulak pelikulak) throws SQLException {
+        JPanel informazioa = new JPanel(new GridLayout(5, 1));
 
-        informazioa.setLayout(new GridLayout(5,1));
+        // Genero
+        String generoa = pelikulak.getGeneroa().replace(";", " - ");
+        JLabel generoLabel = new JLabel("Generoa: " + generoa);
+        informazioa.add(generoLabel);
 
-
-        String generoa = pelikulak.getGeneroa();
-        generoa = generoa.replace(";", " - ");
-        JLabel izenBuru = new JLabel(generoa);
-        informazioa.add(izenBuru);
-
-
+        // Estreno
         Konexioa conn = new Konexioa();
         ResultSet emaitza = conn.estrenaldia(pelikulak.getIdEstrenaldia());
         emaitza.next();
-
-        String data = String.valueOf(emaitza.getString("ESTREINALDI_DATA"));
-        //System.out.println(data);
+        String data = emaitza.getString("ESTREINALDI_DATA");
         conn.konexioaItxi();
+        JLabel dataLabel = new JLabel("Estrenaldia: " + data);
+        informazioa.add(dataLabel);
 
+        // Irabaziak
+        JLabel irabaziakLabel = new JLabel("Irabaziak: " + pelikulak.getIrabaziak());
+        informazioa.add(irabaziakLabel);
 
+        // Aurrekontua
+        JLabel aurrekontuaLabel = new JLabel("Aurrekontua: " + pelikulak.getAurrekontua());
+        informazioa.add(aurrekontuaLabel);
 
-        JLabel dat = new JLabel(data);
-        informazioa.add(dat);
-
-
-        JLabel irabaziak = new JLabel(String.valueOf(pelikulak.getIrabaziak()));
-        informazioa.add(irabaziak);
-
-        JLabel aurrekontua = new JLabel(String.valueOf(pelikulak.getAurrekontua()));
-        informazioa.add(aurrekontua);
-
-
+        // Trailer
         JButton trailera = new JButton("Trailera");
         trailera.addActionListener(new ActionListener() {
             @Override
@@ -105,23 +121,16 @@ public class PelikulaInformazioa {
                 }
             }
         });
-
         informazioa.add(trailera);
 
         return informazioa;
-
     }
 
-
-    static JLabel irudiak(String url, int x, int y){
+    static JLabel irudiak(String url, int x, int y) {
         ImageIcon originalIcon = new ImageIcon(url);
-
         Image img = originalIcon.getImage();
         Image img1 = img.getScaledInstance(x, y, Image.SCALE_SMOOTH);
-        ImageIcon thedark = new ImageIcon(img1);
-
-        JLabel label = new JLabel();
-        label.setIcon(thedark);
-        return label;
+        ImageIcon scaledIcon = new ImageIcon(img1);
+        return new JLabel(scaledIcon);
     }
 }
